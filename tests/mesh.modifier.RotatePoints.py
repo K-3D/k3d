f1b8a42381652de78978cb94570d64c5bf91b00e
Timothy M. Shead <tshead@k-3d.com>
2007-05-21 05:30:59 +0000

    COMP: Added a regression test for RotatePoints

---
+++ tests/mesh.modifier.RotatePoints.py
@@ -0,0 +1,15 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyCube", "RotatePoints")
+
+selection = k3d.deselect_all()
+selection.points = k3d.component_select_all()
+
+setup.modifier.mesh_selection = selection
+setup.modifier.z = 1.0
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.RotatePoints", 0)
+
