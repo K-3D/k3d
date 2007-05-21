e80a51d1efac4af7ee0041d26e833c28c6ce5a5d
Timothy M. Shead <tshead@k-3d.com>
2007-05-21 05:27:47 +0000

    COMP: Added a regression test for LinearPointNoise

---
+++ tests/mesh.modifier.LinearPointNoise.py
@@ -0,0 +1,17 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyGrid", "LinearPointNoise")
+
+selection = k3d.deselect_all()
+selection.points = k3d.component_select_all()
+
+setup.modifier.mesh_selection = selection
+setup.modifier.amplitude_x = 2
+setup.modifier.amplitude_y = 2
+setup.modifier.amplitude_z = 2
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.LinearPointNoise", 0)
+
