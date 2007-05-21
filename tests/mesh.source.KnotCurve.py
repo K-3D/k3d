b771639ec44b38a4bc65b79305d1890e49db8873
Timothy M. Shead <tshead@k-3d.com>
2007-05-21 05:58:12 +0000

    COMP: Fix some regression-test false positives on Win32

--- tests/mesh.source.KnotCurve.py
+++ tests/mesh.source.KnotCurve.py
@@ -3,5 +3,5 @@
 import testing
 
 setup = testing.setup_mesh_source_test("KnotCurve")
-testing.mesh_comparison(setup.document, setup.source.get_property("output_mesh"), "mesh.source.KnotCurve", 1)
+testing.mesh_comparison(setup.document, setup.source.get_property("output_mesh"), "mesh.source.KnotCurve", 2)
 
