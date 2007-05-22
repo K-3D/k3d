03aedde68e581a1c060b2302e163747dc44e4adc
Timothy M. Shead <tshead@k-3d.com>
2007-05-22 03:49:49 +0000

    Added a regression test for SmoothPoints

---
+++ tests/mesh.modifier.SmoothPoints.py
@@ -0,0 +1,18 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyCube", "SmoothPoints")
+
+setup.source.rows = 2
+setup.source.columns = 2
+setup.source.slices = 2
+
+selection = k3d.deselect_all()
+selection.points = k3d.component_select_all()
+
+setup.modifier.mesh_selection = selection
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.SmoothPoints", 1)
+
