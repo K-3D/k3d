b5d9cdfc3382dde03327636c3ec0c9560c5eff63
Timothy M. Shead <tshead@k-3d.com>
2007-05-22 03:56:05 +0000

    COMP: Added a regression test for TaperPoints

---
+++ tests/mesh.modifier.TaperPoints.py
@@ -0,0 +1,15 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyCylinder", "TaperPoints")
+
+selection = k3d.deselect_all()
+selection.points = k3d.component_select_all()
+
+setup.modifier.mesh_selection = selection
+setup.modifier.taper_factor = 1
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.TaperPoints", 1)
+
