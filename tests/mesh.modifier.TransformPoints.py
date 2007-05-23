08cda26a235a9f4da7b026aca26ead2851d1e44a
Timothy M. Shead <tshead@k-3d.com>
2007-05-23 00:18:54 +0000

    COMP: Added a regression test for TransformPoints

---
+++ tests/mesh.modifier.TransformPoints.py
@@ -0,0 +1,15 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyCube", "TransformPoints")
+
+selection = k3d.deselect_all()
+selection.points = k3d.component_select_all()
+
+setup.modifier.mesh_selection = selection
+setup.modifier.input_matrix = k3d.translate3(k3d.vector3(0, 0, 1))
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.TransformPoints", 1)
+
