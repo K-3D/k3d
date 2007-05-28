43bd3caf826c0d5cc784a2c9d8200b31826e77b0
Timothy M. Shead <tshead@k-3d.com>
2007-05-28 15:50:10 +0000

    COMP: More build cleanup on OSX, use pkg-config for libsigc++ configuration, enable the glx and offscreen modules

--- cmake/modules/FindSigC.cmake
+++ cmake/modules/FindSigC.cmake
@@ -1,32 +1,67 @@
-ADD_DEFINITIONS(-DK3D_HAVE_SIGC_2_0)
-FIND_PATH(K3D_SIGC_CONFIG_INCLUDE_DIR sigc++config.h
-	/usr/lib/sigc++-2.0/include
-	/opt/local/lib/sigc++-2.0/include
-	c:/gtk/lib/sigc++-2.0/include
-	DOC "Directory where the sigc++ config file is located"
-	)
-MARK_AS_ADVANCED(K3D_SIGC_CONFIG_INCLUDE_DIR)
+SET(K3D_SIGC_FOUND 0)
 
-FIND_PATH(K3D_SIGC_INCLUDE_DIR sigc++
-	/usr/include/sigc++-2.0
-	/opt/local/include/sigc++-2.0
-	c:/gtk/include/sigc++-2.0
-	DOC "Directory where the sigc++ header files are located"
-	)
-MARK_AS_ADVANCED(K3D_SIGC_INCLUDE_DIR)
 
-SET(K3D_SIGC_LIB sigc-2.0 CACHE STRING "")
-MARK_AS_ADVANCED(K3D_SIGC_LIB)
+######################################################################
+# Posix specific configuration
 
-# Following are the consolidated variables that should be used for builds
-SET(K3D_SIGC_INCLUDE_DIRS
-	${K3D_SIGC_CONFIG_INCLUDE_DIR}
-	${K3D_SIGC_INCLUDE_DIR}
-	)
+IF(UNIX)
+	INCLUDE(FindPkgConfig)
+	PKG_CHECK_MODULES(SIGC sigc++-2.0)
 
-SET(K3D_SIGC_LIB_DIRS
-	${K3D_GTK_LIB_DIR}
-	)
+	IF(SIGC_FOUND)
+		SET(K3D_SIGC_INCLUDE_DIRS
+			${SIGC_INCLUDE_DIRS}
+			)
 
-SET(K3D_SIGC_FOUND 1)
+		SET(K3D_SIGC_LIB_DIRS
+			${SIGC_LIBRARY_DIRS}
+			)
+
+		SET(K3D_SIGC_LIBS
+			${SIGC_LIBRARIES}
+			)
+
+		SET(K3D_SIGC_FOUND 1)
+
+		ADD_DEFINITIONS(-DK3D_HAVE_SIGC_2_0)
+
+	ENDIF(SIGC_FOUND)
+
+ENDIF(UNIX)
+
+IF(WIN32)
+	FIND_PATH(K3D_SIGC_CONFIG_INCLUDE_DIR sigc++config.h
+		c:/gtk/lib/sigc++-2.0/include
+		DOC "Directory where the sigc++ config file is located"
+		)
+	MARK_AS_ADVANCED(K3D_SIGC_CONFIG_INCLUDE_DIR)
+
+	FIND_PATH(K3D_SIGC_INCLUDE_DIR sigc++
+		c:/gtk/include/sigc++-2.0
+		DOC "Directory where the sigc++ header files are located"
+		)
+	MARK_AS_ADVANCED(K3D_SIGC_INCLUDE_DIR)
+
+	SET(K3D_SIGC_LIB sigc-2.0 CACHE STRING "")
+	MARK_AS_ADVANCED(K3D_SIGC_LIB)
+
+	# Following are the consolidated variables that should be used for builds
+	SET(K3D_SIGC_INCLUDE_DIRS
+		${K3D_SIGC_CONFIG_INCLUDE_DIR}
+		${K3D_SIGC_INCLUDE_DIR}
+		)
+
+	SET(K3D_SIGC_LIB_DIRS
+		${K3D_GTK_LIB_DIR}
+		)
+
+	SET(K3D_SIGC_LIBS
+		${K3D_SIGC_LIB}
+		)
+
+	SET(K3D_SIGC_FOUND 1)
+
+	ADD_DEFINITIONS(-DK3D_HAVE_SIGC_2_0)
+
+ENDIF(WIN32)
 
