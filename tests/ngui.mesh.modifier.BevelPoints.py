0f63ca7f2b783da765c1deef11147e303af4ded2
Timothy M. Shead <tshead@k-3d.com>
2007-06-15 01:09:57 +0000

    COMP: Revert inadvertent commit

--- tests/ngui.mesh.modifier.BevelPoints.py
+++
@@ -1,7 +0,0 @@
-#python
-
-import k3d
-
-k3d.get_command_node("/document/window/toolbar/create/create_PolyCube").execute_command("activate", "")
-k3d.get_command_node("/document/window/menus/mesh_modifier_BevelPoints").execute_command("activate", "")
-k3d.get_command_node("/document/window/node_properties/offset").execute_command("set_value", "0.4")
