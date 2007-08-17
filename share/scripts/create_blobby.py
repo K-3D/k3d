d7dd3d8a4d49949f091b08ba04babd564a7a0335
Timothy M. Shead <tshead@k-3d.com>
2007-08-17 03:56:27 +0000

    BUG: Fix broken create_blobby.py sample script

--- share/scripts/create_blobby.py
+++ share/scripts/create_blobby.py
@@ -8,31 +8,71 @@ try:
 	frozen_mesh = doc.new_node("FrozenMesh")
 	frozen_mesh.name = "Blobby"
 
-	mesh = frozen_mesh.new_mesh()
-	positions = ((-.5, .5, 0), (.5, .5, 0), (.5, -.5, 0), (-.5, -.5, 0))
-	for position in positions:
-		mesh.new_point(position)
+	mesh = k3d.dynamic_cast(frozen_mesh, "imesh_storage").reset_mesh()
 
-	blobby = mesh.new_blobby()
+	ellipsoids = [k3d.point3(-1, 0, 1), k3d.point3(1, 0, 1), k3d.point3(1, 0, -1), k3d.point3(-1, 0, -1)]
 
-	ellipsoids = []
-	for point in mesh.points:
-		ellipsoids.append(blobby.new_ellipsoid(point))
+	blobbies = mesh.create_blobbies()
 
-	blobby.root = blobby.new_add(ellipsoids)
+	first_primitives = blobbies.create_first_primitives()
+	first_primitives.append(0)
+
+	primitive_counts = blobbies.create_primitive_counts()
+	primitive_counts.append(len(ellipsoids) + 1)
+
+	first_operators = blobbies.create_first_operators()
+	first_operators.append(0)
+
+	operator_counts = blobbies.create_operator_counts()
+	operator_counts.append(1)
+
+	materials = blobbies.create_materials()
+	materials.append(k3d.dynamic_cast(Document.get_node("Material"), "imaterial"))
+
+	primitives = blobbies.create_primitives()
+	primitive_first_floats = blobbies.create_primitive_first_floats()
+	primitive_float_counts = blobbies.create_primitive_float_counts()
+	floats = blobbies.create_floats()
+
+	for center in ellipsoids:
+		primitives.append(k3d.primitive_type.ellipsoid)
+		primitive_first_floats.append(len(floats))
+		primitive_float_counts.append(16)
+		for i in (k3d.translate3(center) * k3d.scale3(1, 1, 1)).column_major_list():
+			floats.append(i)
+
+	primitives.append(k3d.primitive_type.segment)
+	primitive_first_floats.append(len(floats))
+	primitive_float_counts.append(23)
+	floats.append(-1)
+	floats.append(0)
+	floats.append(0)
+	floats.append(1)
+	floats.append(0)
+	floats.append(0)
+	floats.append(1)
+	for i in k3d.identity3().column_major_list():
+		floats.append(i)
+
+	Cs = blobbies.writable_varying_data().create_array("Cs", "k3d::color")
+	Cs.assign([k3d.color(1, 0, 0), k3d.color(0, 1, 0), k3d.color(0, 0, 1), k3d.color(1, 1, 0), k3d.color(1, 1, 1)])
+
+	operators = blobbies.create_operators()
+	operator_first_operands = blobbies.create_operator_first_operands()
+	operator_operand_counts = blobbies.create_operator_operand_counts()
+	operands = blobbies.create_operands()
+
+	operators.append(k3d.operator_type.add)
+	operator_first_operands.append(len(operands))
+	operator_operand_counts.append(6)
+	operands.assign([5, 0, 1, 2, 3, 4])
+
 
 	mesh_instance = doc.new_node("MeshInstance")
 	mesh_instance.name = "Blobby Instance"
-
-	scale = doc.new_node("Scale")
-	scale.name = "Blobby Scale"
-	scale.x = 5.0
-	scale.y = 5.0
-	scale.z = 5.0
-
-#	mesh_instance.scale = (5, 5, 5)
+	mesh_instance.gl_painter = doc.get_node("GL Default Painter")
+	mesh_instance.ri_painter = doc.get_node("RenderMan Default Painter")
 	doc.set_dependency(mesh_instance.get_property("input_mesh"), frozen_mesh.get_property("output_mesh"))
-	doc.set_dependency(mesh_instance.get_property("input_matrix"), scale.get_property("output_matrix"))
 
 	doc.finish_change_set("Create Test Blobby")
 
