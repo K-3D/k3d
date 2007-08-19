bee3e4954880f4a5aa9412b9f5d90c5f5f808c43
Timothy M. Shead <tshead@k-3d.com>
2007-08-19 21:39:19 +0000

    COMP: Create additional test cases for the TriangulateFaces modifier

---
+++ tests/mesh.modifier.TriangulateFaces.PolyGrid.selection.py
@@ -0,0 +1,15 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyGrid", "TriangulateFaces")
+setup.source.rows = 3
+setup.source.columns = 3
+
+mesh_selection = k3d.deselect_all()
+mesh_selection.faces = [ (0, 3, 1) ]
+setup.modifier.mesh_selection = mesh_selection
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.TriangulateFaces.PolyGrid.selection", 0)
+
