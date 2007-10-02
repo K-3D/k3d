93494bc716300f853f58947d8bd26f377d2bf796
Timothy M. Shead <tshead@k-3d.com>
2007-10-02 05:36:35 +0000

    BUG: Python array wrappers were out-of-sync with the new fundamental types on some platforms

--- k3dsdk/python/array_python.cpp
+++ k3dsdk/python/array_python.cpp
@@ -36,31 +36,31 @@ namespace python
 
 void export_arrays()
 {
-	export_array<k3d::typed_array<bool> >("bool_array",
+	export_array<k3d::typed_array<k3d::bool_t> >("k3d_bool_t_array",
 		"Stores a mutable (read-write) collection of boolean values.");
-	export_array<k3d::typed_array<double> >("double_array",
+	export_array<k3d::typed_array<k3d::double_t> >("k3d_double_t_array",
 		"Stores a mutable (read-write) collection of floating-point values.");
-	export_array<k3d::typed_array<k3d::mesh::polyhedra_t::polyhedron_type> >("polyhedron_type_array",
+	export_array<k3d::typed_array<k3d::uint_t> >("k3d_uint_t_array",
+		"Stores a mutable (read-write) collection of unsigned integer values.");
+	export_array<k3d::typed_array<k3d::mesh::polyhedra_t::polyhedron_type> >("k3d_mesh_polyheda_t_polyhedron_type_array",
 		"Stores a mutable (read-write) collection of polyhedron type values.");
-	export_array<k3d::typed_array<k3d::mesh::blobbies_t::operator_type> >("blobby_operator_type_array",
+	export_array<k3d::typed_array<k3d::mesh::blobbies_t::operator_type> >("k3d_mesh_blobbies_t_operator_type_array",
 		"Stores a mutable (read-write) collection of blobby operator type values.");
-	export_array<k3d::typed_array<k3d::mesh::blobbies_t::primitive_type> >("blobby_primitive_type_array",
+	export_array<k3d::typed_array<k3d::mesh::blobbies_t::primitive_type> >("k3d_mesh_blobbies_t_primitive_type_array",
 		"Stores a mutable (read-write) collection of blobby primitive type values.");
-	export_array<k3d::typed_array<k3d::imaterial*> >("material_array",
+	export_array<k3d::typed_array<k3d::imaterial*> >("k3d_imaterial_array",
 		"Stores a mutable (read-write) collection of L{imaterial} objects.");
-	export_array<k3d::typed_array<k3d::color> >("color_array",
+	export_array<k3d::typed_array<k3d::color> >("k3d_color_array",
 		"Stores a mutable (read-write) collection of L{color} values.");
-	export_array<k3d::typed_array<k3d::matrix4> >("matrix4_array",
+	export_array<k3d::typed_array<k3d::matrix4> >("k3d_matrix4_array",
 		"Stores a mutable (read-write) collection of L{matrix4} values.");
-	export_array<k3d::typed_array<k3d::normal3> >("normal3_array",
+	export_array<k3d::typed_array<k3d::normal3> >("k3d_normal3_array",
 		"Stores a mutable (read-write) collection of L{normal3} values.");
-	export_array<k3d::typed_array<k3d::point3> >("point3_array",
+	export_array<k3d::typed_array<k3d::point3> >("k3d_point3_array",
 		"Stores a mutable (read-write) collection of L{point3} values.");
-	export_array<k3d::typed_array<k3d::point4> >("point4_array",
+	export_array<k3d::typed_array<k3d::point4> >("k3d_point4_array",
 		"Stores a mutable (read-write) collection of L{point4} values.");
-	export_array<k3d::typed_array<size_t> >("size_t_array",
-		"Stores a mutable (read-write) collection of integer values.");
-	export_array<k3d::typed_array<k3d::vector3> >("vector3_array",
+	export_array<k3d::typed_array<k3d::vector3> >("k3d_vector3_array",
 		"Stores a mutable (read-write) collection of L{vector3} values.");
 }
 
--- k3dsdk/python/const_array_python.cpp
+++ k3dsdk/python/const_array_python.cpp
@@ -36,31 +36,31 @@ namespace python
 
 void export_const_arrays()
 {
-	export_const_array<const k3d::typed_array<bool> >("const_bool_array",
+	export_const_array<const k3d::typed_array<k3d::bool_t> >("const_k3d_bool_t_array",
 		"Stores an immutable (read-only) collection of boolean values.");
-	export_const_array<const k3d::typed_array<double> >("const_double_array",
+	export_const_array<const k3d::typed_array<k3d::double_t> >("const_k3d_double_t_array",
 		"Stores an immutable (read-only) collection of floating-point values.");
-	export_const_array<const k3d::typed_array<k3d::mesh::polyhedra_t::polyhedron_type> >("const_polyhedron_type_array",
-		"Stores an immutable (read-only) collection of polyhedron type values.");
-	export_const_array<const k3d::typed_array<k3d::mesh::blobbies_t::operator_type> >("const_blobby_operator_type_array",
-		"Stores an immutable (read-only) collection of blobby operator type values.");
-	export_const_array<const k3d::typed_array<k3d::mesh::blobbies_t::primitive_type> >("const_blobby_primitive_type_array",
-		"Stores an immutable (read-only) collection of blobby primitive type values.");
-	export_const_array<const k3d::typed_array<k3d::imaterial*> >("const_material_array",
-		"Stores an immutable (read-only) collection of L{imaterial} objects.");
-	export_const_array<const k3d::typed_array<k3d::color> >("const_color_array",
-		"Stores an immutable (read-only) collection of L{color} values.");
-	export_const_array<const k3d::typed_array<k3d::matrix4> >("const_matrix4_array",
-		"Stores an immutable (read-only) collection of L{matrix4} values.");
-	export_const_array<const k3d::typed_array<k3d::normal3> >("const_normal3_array",
-		"Stores an immutable (read-only) collection of L{normal3} values.");
-	export_const_array<const k3d::typed_array<k3d::point3> >("const_point3_array",
-		"Stores an immutable (read-only) collection of L{point3} values.");
-	export_const_array<const k3d::typed_array<k3d::point4> >("const_point4_array",
-		"Stores an immutable (read-only) collection of L{point4} values.");
-	export_const_array<const k3d::typed_array<size_t> >("const_size_t_array",
+	export_const_array<const k3d::typed_array<k3d::uint_t> >("const_k3d_uint_t_array",
 		"Stores an immutable (read-only) collection of integer values.");
-	export_const_array<const k3d::typed_array<k3d::vector3> >("const_vector3_array",
+	export_const_array<const k3d::typed_array<k3d::mesh::polyhedra_t::polyhedron_type> >("const_k3d_mesh_polyhedra_t_polyhedron_type_array",
+		"Stores an immutable (read-only) collection of polyhedron type values.");
+	export_const_array<const k3d::typed_array<k3d::mesh::blobbies_t::operator_type> >("const_k3d_mesh_blobbies_t_operator_type_array",
+		"Stores an immutable (read-only) collection of blobby operator type values.");
+	export_const_array<const k3d::typed_array<k3d::mesh::blobbies_t::primitive_type> >("const_k3d_mesh_blobbies_t_primitive_type_array",
+		"Stores an immutable (read-only) collection of blobby primitive type values.");
+	export_const_array<const k3d::typed_array<k3d::imaterial*> >("const_k3d_imaterial_array",
+		"Stores an immutable (read-only) collection of L{imaterial} objects.");
+	export_const_array<const k3d::typed_array<k3d::color> >("const_k3d_color_array",
+		"Stores an immutable (read-only) collection of L{color} values.");
+	export_const_array<const k3d::typed_array<k3d::matrix4> >("const_k3d_matrix4_array",
+		"Stores an immutable (read-only) collection of L{matrix4} values.");
+	export_const_array<const k3d::typed_array<k3d::normal3> >("const_k3d_normal3_array",
+		"Stores an immutable (read-only) collection of L{normal3} values.");
+	export_const_array<const k3d::typed_array<k3d::point3> >("const_k3d_point3_array",
+		"Stores an immutable (read-only) collection of L{point3} values.");
+	export_const_array<const k3d::typed_array<k3d::point4> >("const_k3d_point4_array",
+		"Stores an immutable (read-only) collection of L{point4} values.");
+	export_const_array<const k3d::typed_array<k3d::vector3> >("const_k3d_vector3_array",
 		"Stores an immutable (read-only) collection of L{vector3} values.");
 }
 
