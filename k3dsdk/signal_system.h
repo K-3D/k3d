9419493168c30556958d7410ebbb6598466ac0ea
Timothy M. Shead <tshead@k-3d.com>
2007-10-19 04:11:51 +0000

    COMP: Update our sigc++ includes to work with (apparent) changes in 2.1

--- k3dsdk/signal_system.h
+++ k3dsdk/signal_system.h
@@ -20,10 +20,7 @@
 // License along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#include <sigc++/bind.h>
-#include <sigc++/connection.h>
-#include <sigc++/hide.h>
-#include <sigc++/signal.h>
+#include <sigc++/sigc++.h>
 
 #endif // !K3DSDK_SIGNAL_SYSTEM_H
 
