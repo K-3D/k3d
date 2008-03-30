29d76f64715db0f8ab7f358980e2da343bb35f9c
Timothy M. Shead <tshead@k-3d.com>
2008-03-30 19:30:16 +0000

    ENH: provide better feedback from the document.stress.all_node_types.py test.

--- tests/document.stress.all_node_types.py
+++ tests/document.stress.all_node_types.py
@@ -1,10 +1,15 @@
 #python
 
 import k3d
+import sys
+
 doc = k3d.new_document();
 
 factories = k3d.plugins();
 for factory in factories:
 	if factory.is_document_plugin():
+		sys.stderr.write(factory.name() + "\n")
+		sys.stderr.flush()
+
 		node = doc.new_node(factory)
 
