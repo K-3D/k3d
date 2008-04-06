17180226a1de7007ec54eb95546ba36233da6861
Bart Janssens <bart.janssens@lid.kviv.be>
2008-04-06 19:09:04 +0000

    BUG: Make CGAL booleans produce the same output across runs and platforms

--- modules/booleans/conversion.cpp
+++ modules/booleans/conversion.cpp
@@ -25,6 +25,8 @@
 #include "conversion.h"
 #include "k3d_to_nef.h"
 
+#include <k3dsdk/gprim_factory.h>
+
 #include <map>
 
 namespace libk3dbooleans
@@ -107,147 +109,87 @@ void to_mesh(const Polyhedron& Polyhedron, k3d::mesh& Mesh, k3d::imaterial* cons
   Mesh.polyhedra = polyhedra;
 }
 
-// helper types and functions for to_mesh
-typedef std::map<const Nef_polyhedron::Vertex*, size_t> vertices_t;
-typedef std::pair<size_t, size_t> edge_t;
-typedef std::vector<edge_t> edges_t;
-typedef std::vector<edges_t> edges_per_facet_t;
-typedef std::map<const Nef_polyhedron::Halffacet*, size_t> facet_map_t;
+// Note: Nef_polyhedron to mesh conversion based on the to_Polyhedron_3 conversion from Nef_polyhedron_3.h in the CGAL distribution,
+// so part is (c) the authors of that file.
 
-size_t get_vertex_number(const Nef_polyhedron::Vertex* Vertex, vertices_t& Vertices, boost::shared_ptr<k3d::mesh::points_t>& Points)
+/// Visitor class to help building the k3d mesh
+class nef_visitor
 {
-	std::pair<vertices_t::iterator, bool> insert_result = Vertices.insert(std::make_pair(Vertex, Points->size()));
-	if (insert_result.second)
+public:
+	nef_visitor(k3d::gprim_factory& Factory, const CGAL::Object_index<Nef_polyhedron::Vertex_const_iterator>& VertexIndices)
+	: m_factory(Factory), m_vertex_indices(VertexIndices) {}
+	
+	void visit(Nef_polyhedron::Halffacet_const_handle OppositeFacet)
 	{
-		Point_3 mesh_point = Vertex->point();
-		Points->push_back(k3d::point3(CGAL::to_double(mesh_point.x()), CGAL::to_double(mesh_point.y()), CGAL::to_double(mesh_point.z()))); 
+		return_if_fail(Nef_polyhedron::Infi_box::is_standard(OppositeFacet->plane()));
+		
+		Nef_polyhedron::SHalfedge_const_handle se;
+		Nef_polyhedron::Halffacet_cycle_const_iterator fc;
+		     	
+		Nef_polyhedron::Halffacet_const_handle f = OppositeFacet->twin();
+		
+		k3d::mesh::indices_t corners;
+		fc = f->facet_cycles_begin();
+		se = Nef_polyhedron::SHalfedge_const_handle(fc);
+		return_if_fail(se!=0);
+		Nef_polyhedron::SHalfedge_around_facet_const_circulator hc_start(se);
+		Nef_polyhedron::SHalfedge_around_facet_const_circulator hc_end(hc_start);
+		CGAL_For_all(hc_start,hc_end)
+		{
+			corners.push_back(m_vertex_indices[hc_start->source()->center_vertex()]);
+		}
+		m_factory.add_polygon(corners);
+		++fc;
+		CGAL_For_all(fc, f->facet_cycles_end())
+		{
+			k3d::mesh::indices_t hole_corners;
+			se = Nef_polyhedron::SHalfedge_const_handle(fc);
+			return_if_fail(se!=0);
+			Nef_polyhedron::SHalfedge_around_facet_const_circulator hole_start(se);
+			Nef_polyhedron::SHalfedge_around_facet_const_circulator hole_end(hole_start);
+			CGAL_For_all(hole_start,hole_end)
+			{
+				hole_corners.push_back(m_vertex_indices[hole_start->source()->center_vertex()]);
+			}
+			m_factory.add_hole(hole_corners);
+		}
 	}
-	return insert_result.first->second;
-}
+	
+	void visit(Nef_polyhedron::SFace_const_handle) {}
+  void visit(Nef_polyhedron::Halfedge_const_handle) {}
+  void visit(Nef_polyhedron::Vertex_const_handle) {}
+  void visit(Nef_polyhedron::SHalfedge_const_handle) {}
+  void visit(Nef_polyhedron::SHalfloop_const_handle) {}
+	
+private:
+	k3d::gprim_factory& m_factory;
+	const CGAL::Object_index<Nef_polyhedron::Vertex_const_iterator>& m_vertex_indices;
+};
 
 void to_mesh(Nef_polyhedron& NefPolyhedron, k3d::mesh& Mesh, k3d::imaterial* const Material)
-{
-	boost::shared_ptr<k3d::mesh::points_t> points(new k3d::mesh::points_t());
-  boost::shared_ptr<k3d::mesh::polyhedra_t> polyhedra(new k3d::mesh::polyhedra_t());
-	boost::shared_ptr<k3d::mesh::indices_t> first_faces(new k3d::mesh::indices_t());
-	boost::shared_ptr<k3d::mesh::counts_t> face_counts(new k3d::mesh::counts_t());
-	boost::shared_ptr<k3d::mesh::polyhedra_t::types_t> types(new k3d::mesh::polyhedra_t::types_t());
-	boost::shared_ptr<k3d::mesh::indices_t> face_first_loops(new k3d::mesh::indices_t());
-	boost::shared_ptr<k3d::mesh::counts_t> face_loop_counts(new k3d::mesh::counts_t());
-	boost::shared_ptr<k3d::mesh::selection_t> face_selection(new k3d::mesh::selection_t());
-	boost::shared_ptr<k3d::mesh::indices_t> loop_first_edges(new k3d::mesh::indices_t());
-	boost::shared_ptr<k3d::mesh::indices_t> edge_points(new k3d::mesh::indices_t());
-	boost::shared_ptr<k3d::mesh::indices_t> clockwise_edges(new k3d::mesh::indices_t());
-	boost::shared_ptr<k3d::mesh::selection_t> edge_selection(new k3d::mesh::selection_t());
-  
-  types->push_back(k3d::mesh::polyhedra_t::POLYGONS);
-  
-  vertices_t vertices;
-  facet_map_t facet_map;
-  
-  // Assign a number to each facet, so the facet order does not depend on the facet's memory address
-  size_t num_facets = 0;
-  for (Nef_polyhedron::Halffacet_const_iterator facet = NefPolyhedron.halffacets_begin(); facet != NefPolyhedron.halffacets_end(); ++facet)
-  {
-  	if (&(*facet->incident_volume()) && facet->incident_volume()->mark()) // Only consider facets that enclose the marked volume
-  	{
-  		facet_map.insert(std::make_pair(&(*facet), num_facets)); 
-  		num_facets++;
-  	}
-  }
-  
-  edges_per_facet_t edges_per_facet(num_facets);
-  
-  for (Nef_polyhedron::Halfedge_const_iterator e = NefPolyhedron.halfedges_begin(); e != NefPolyhedron.halfedges_end(); ++e)
-  {
-		const Nef_polyhedron::Vertex* start = &(*e->source());
-		const Nef_polyhedron::Vertex* end = &(*e->target());
-		
-		edge_t edge = std::make_pair(get_vertex_number(start, vertices, points), get_vertex_number(end, vertices, points));
-		
-		// Every halfedge (=svertex) has two outgoing sedges, one of which corresponds to a facet of the enclosed volume (mark==true)
-		if (e->out_sedge()->facet()->incident_volume()->mark()) // First outgoing sedge
-		{
-			edges_per_facet[facet_map[&(*e->out_sedge()->facet())]].push_back(edge);
-		}
-		else // second outgoing sedge
-		{
-			return_if_fail(e->out_sedge()->sprev()->twin()->facet()->incident_volume()->mark());
-			edges_per_facet[facet_map[&(*e->out_sedge()->sprev()->twin()->facet())]].push_back(edge);
-		}
-  }
-  
-  for (size_t facet = 0; facet != edges_per_facet.size(); ++facet)
-  {
-  	edges_t edges = edges_per_facet[facet];
-
-		// First, we need to sort the un-ordered edge list that is stored for each facet so we get the actual edge loops
-  	std::vector<edges_t> cycles;
-  	while (!edges.empty())
-  	{
-  		edges_t cycle;
-  		cycle.push_back(edges.front());
-  		edges.erase(edges.begin());
-  		while (cycle.back().second != cycle.front().first && !edges.empty())
-  		{
-	  		for (edges_t::iterator edge = edges.begin(); edge != edges.end(); )
-	  		{
-	  			if (edge->first == cycle.back().second && edge->second != cycle.back().first)
-	  			{
-	  				cycle.push_back(*edge);
-	  				edge = edges.erase(edge);
-	  			}
-	  			else
-	  			{
-	  				++edge;
-	  			}
-	  		}
-  		}
-  		if (cycle.size() > 2 && cycle.back().second == cycle.front().first)
-  			cycles.push_back(cycle);
-  	}
-  	
-  	size_t face_first_loop = loop_first_edges->size();
-  	
-  	for (size_t i = 0; i != cycles.size(); ++i)
-  	{
-	  	const size_t first_edge = edge_points->size();
-	  	loop_first_edges->push_back(first_edge);
-  		edges_t cycle = cycles[i];
-  		for (edges_t::iterator edge = cycle.begin(); edge != cycle.end(); ++edge)
-  		{
-  			edge_points->push_back(edge->first);
-  			clockwise_edges->push_back(edge_points->size());
-  			edge_selection->push_back(0.0);
-  		}
-  		clockwise_edges->back() = first_edge; 
-  	}
-  	
-   	face_first_loops->push_back(face_first_loop);
-		face_loop_counts->push_back(cycles.size());
-		face_selection->push_back(0.0);   
-  }
-  
-  boost::shared_ptr<k3d::mesh::selection_t> point_selection(new k3d::mesh::selection_t(points->size()));
-  
-  first_faces->push_back(0);
-	face_counts->push_back(face_first_loops->size());
-	boost::shared_ptr<k3d::mesh::materials_t> face_materials(new k3d::mesh::materials_t(face_first_loops->size(), Material));
+{ 
+	k3d::gprim_factory factory(Mesh);
+	k3d::int32_t skip_volumes = Nef_polyhedron::Infi_box::extended_kernel() ? 2 : 1;
 	
-  polyhedra->edge_selection = edge_selection;
-	polyhedra->clockwise_edges = clockwise_edges;
-	polyhedra->edge_points = edge_points;
-	polyhedra->loop_first_edges = loop_first_edges;
-	polyhedra->face_materials = face_materials;
-	polyhedra->face_selection = face_selection;
-	polyhedra->face_loop_counts = face_loop_counts;
-	polyhedra->face_first_loops = face_first_loops;
-	polyhedra->types = types;
-	polyhedra->face_counts = face_counts;
-	polyhedra->first_faces = first_faces;
-  Mesh.points = points;
-  Mesh.point_selection = point_selection;
-  Mesh.polyhedra = polyhedra;
+	Nef_polyhedron::Vertex_const_iterator v;
+	CGAL::Object_index<Nef_polyhedron::Vertex_const_iterator> vertex_indices; 
+	CGAL_forall_vertices(v, NefPolyhedron)
+	{
+		if(Nef_polyhedron::Infi_box::is_standard(v->point()))
+		{
+			Point_3 mesh_point = v->point();
+			factory.add_point(k3d::point3(CGAL::to_double(mesh_point.x()), CGAL::to_double(mesh_point.y()), CGAL::to_double(mesh_point.z())));
+			vertex_indices[v] = Mesh.points->size() - 1;
+		}
+	}
+	
+	nef_visitor V(factory, vertex_indices);
+	Nef_polyhedron::Volume_const_handle c;
+	CGAL_forall_volumes(c, NefPolyhedron)
+	{
+		if(skip_volumes-- <= 0)
+		  NefPolyhedron.visit_shell_objects(Nef_polyhedron::SFace_const_handle(c->shells_begin()),V);
+	}
 }
 
 boost::shared_ptr<Nef_polyhedron> to_nef(const k3d::mesh& Mesh)
