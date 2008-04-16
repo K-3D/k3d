4ac4518ce30d6fe9a965990d396a6b6b6e889018
Timothy M. Shead <tshead@k-3d.com>
2008-04-16 05:06:28 +0000

    BUG: user_interface.py script was broken by the switch from strings to dedicated K-3D path objects.

--- share/scripts/user_interface.py
+++ share/scripts/user_interface.py
@@ -10,8 +10,8 @@ result = k3d.ui().query_message("Pick one!", ["Choice 1", "Choice 2", "Choice 3"
 k3d.ui().message("You chose " + str(result))
 
 result = k3d.ui().get_file_path("read", "test", "Choose file to read (doesn't actually read anything):", "")
-k3d.ui().message("You chose " + result)
+k3d.ui().message("You chose " + str(result))
 
 result = k3d.ui().get_file_path("write", "test", "Choose file to write (doesn't actually write anything):", "")
-k3d.ui().message("You chose " + result)
+k3d.ui().message("You chose " + str(result))
 
