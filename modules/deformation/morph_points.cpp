300acc4889b58713f82fae1c1a8678aabd449aa0
Timothy M. Shead <tshead@k-3d.com>
2008-05-10 02:01:06 +0000

    ENH: cleanup the MorphPoints modifier.

--- modules/deformation/morph_points.cpp
+++ modules/deformation/morph_points.cpp
@@ -18,16 +18,15 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /** \file
-		\author Tim Shead (tshead@k-3d.com) parts of code
-		\author Joaquin Duo (joaduo at lugmen org ar)
+	\author Tim Shead (tshead@k-3d.com) parts of code
+	\author Joaquin Duo (joaduo at lugmen org ar)
 */
 
+#include <k3d-i18n-config.h>
 #include <k3dsdk/algebra.h>
 #include <k3dsdk/document_plugin_factory.h>
-#include <k3d-i18n-config.h>
-#include <k3dsdk/mesh_simple_deformation_modifier.h>
-
 #include <k3dsdk/iuser_property.h>
+#include <k3dsdk/mesh_simple_deformation_modifier.h>
 #include <k3dsdk/properties.h>
 #include <k3dsdk/type_registry.h>
 #include <k3dsdk/user_property_changed_signal.h>
@@ -49,27 +48,16 @@ class morph_points :
 public:
 	morph_points(k3d::iplugin_factory& Factory, k3d::idocument& Document) :
 		base(Factory, Document),
-		m_hundred_limit(init_owner(*this) + init_name("hundred_limit") + init_label(_("100% Limit")) + init_description(_("Morph up to 100%")) + init_value(true)),
-		m_input_mesh1(init_owner(*this) + init_name("input_mesh1") + init_label(_("Target")) + init_description(_("First Morph Target. You may add more target through user properties. Add a Mesh and a Scalar")) + init_value<k3d::mesh*>(0)),
-		m_input_mesh1_percent(init_owner(*this) + init_name("input_mesh1_percent") + init_label(_("Morph Percent")) + init_description(_("First Target Morph Percent. You may add more target through user properties. Add a Mesh and a Scalar")) + init_value(0.)),
 		m_user_property_changed_signal(*this)
 	{
 		m_mesh_selection.changed_signal().connect(make_update_mesh_slot());
-
-		m_hundred_limit.changed_signal().connect(make_update_mesh_slot());
-		m_input_mesh1.changed_signal().connect(make_update_mesh_slot());
-		m_input_mesh1_percent.changed_signal().connect(make_update_mesh_slot());
-		
 		m_user_property_changed_signal.connect(make_update_mesh_slot());
 	}
 
 	void on_deform_mesh(const k3d::mesh::points_t& InputPoints, const k3d::mesh::selection_t& PointSelection, k3d::mesh::points_t& OutputPoints)
 	{
-		const k3d::bool_t hundred_limit = m_hundred_limit.pipeline_value();
-		k3d::mesh &input_mesh1 = *m_input_mesh1.pipeline_value();
-		k3d::double_t input_mesh1_percent = m_input_mesh1_percent.pipeline_value() / 100.;
-		std::vector<k3d::mesh*> morph_targets;
-		std::vector<k3d::double_t> morph_percents;
+		std::vector<const k3d::mesh::points_t*> inputs;
+		std::vector<k3d::double_t> amounts;
 		
 		//Search for the Meshes targets and its morph percent multiplier
 		const k3d::iproperty_collection::properties_t& properties = k3d::node::properties();
@@ -80,11 +68,15 @@ public:
 			
 			if((**property).property_type() == typeid(k3d::mesh*))
 			{
-				morph_targets.push_back( k3d::property::pipeline_value<k3d::mesh*>(**property) );
+				if(k3d::mesh* const mesh = k3d::property::pipeline_value<k3d::mesh*>(**property))
+				{
+					if(const k3d::mesh::points_t* const points = mesh->points.get())
+						inputs.push_back(points);
+				}
 			}
 			else if((**property).property_type() == typeid(k3d::double_t))
 			{
-				morph_percents.push_back( k3d::property::pipeline_value<k3d::double_t>(**property) / 100.);
+				amounts.push_back(k3d::property::pipeline_value<k3d::double_t>(**property));
 			}
 			else
 			{
@@ -92,66 +84,25 @@ public:
 			}
 		}
 
-		if(hundred_limit) //do not exceed 100% limit, all target percent < -1.0;1.0 >
+		if(inputs.size() != amounts.size())
 		{
-			if(input_mesh1_percent > 1.)
-				input_mesh1_percent= 1.;
-			else if(input_mesh1_percent < -1.)
-				input_mesh1_percent= -1.;
-			
-			const size_t index_end = morph_percents.size();
-			for(size_t index = 0; index < index_end; ++index)
-			{
-				if(morph_percents[index] > 1.)
-					morph_percents[index]= 1.;
-				else if(morph_percents[index] < -1.)
-					morph_percents[index]= -1.;
-			}
+			k3d::log() << error << "mismatch in number of morph inputs and amounts." << std::endl;
+			return;
 		}
-		
-		if(&input_mesh1 && (input_mesh1_percent != 0.))
+
+		OutputPoints = InputPoints;
+
+		const k3d::uint_t input_begin = 0;
+		const k3d::uint_t input_end = input_begin + inputs.size();
+		for(k3d::uint_t i = input_begin; i != input_end; ++i)
 		{
-			k3d::mesh::points_t& TargetPoints = const_cast<k3d::mesh::points_t&>(*input_mesh1.points);
-			
-			size_t point_end = OutputPoints.size() < TargetPoints.size() ? OutputPoints.size() : TargetPoints.size();
-			for(size_t point = 0; point != point_end; ++point)//
-			{
-				OutputPoints[point] = k3d::mix(InputPoints[point], InputPoints[point] + (TargetPoints[point] - InputPoints[point])*input_mesh1_percent, PointSelection[point] );
-			}
-			if( TargetPoints.size() < OutputPoints.size()	)
-			{
-				point_end = OutputPoints.size();
-				for(size_t point = TargetPoints.size(); point != point_end; ++point)//
-				{
-					OutputPoints[point] = InputPoints[point];
-				}
-			}
-		}
-  	else
-		{
-			const size_t point_end = OutputPoints.size();
-			for(size_t point = 0; point != point_end; ++point)//
-			{
-				OutputPoints[point] = InputPoints[point];
-			}
-		}
-		
-  	k3d::uint64_t index_end = morph_percents.size() < morph_targets.size() ? morph_percents.size() : morph_targets.size();
-		for(k3d::uint64_t index = 0; index < index_end; ++index)
-		{
-			k3d::mesh& morph_target = *morph_targets[index];
-			k3d::double_t morph_percent = morph_percents[index];
-			if(!&morph_target || !morph_percent)
-				continue;
-				
-			k3d::mesh::points_t& TargetPoints = const_cast<k3d::mesh::points_t&>(*morph_target.points);
-			
-			const size_t point_end = OutputPoints.size() < TargetPoints.size() ? OutputPoints.size() : TargetPoints.size();
-			for(size_t point = 0; point != point_end; ++point)//
-			{
-				OutputPoints[point] = k3d::mix(InputPoints[point], InputPoints[point] + (TargetPoints[point] - InputPoints[point])*morph_percent, PointSelection[point] )
-															- InputPoints[point] + OutputPoints[point] ;
-			}
+			const k3d::mesh::points_t& input_points = *inputs[i];
+			const k3d::double_t amount = amounts[i];
+
+			const k3d::uint_t point_begin = 0;
+			const k3d::uint_t point_end = point_begin + std::min(OutputPoints.size(), input_points.size());
+			for(k3d::uint_t point = point_begin; point != point_end; ++point)
+				OutputPoints[point] += PointSelection[point] * amount * (input_points[point] - InputPoints[point]);
 		}
 	}
 
@@ -170,9 +121,6 @@ public:
 	}
 
 private:
-	k3d_data(k3d::bool_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_hundred_limit; 
-	k3d_data(k3d::mesh*, immutable_name, change_signal, no_undo, local_storage, no_constraint, read_only_property, no_serialization) m_input_mesh1; //First target for user friendlines
-	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_input_mesh1_percent; //First target for user friendlines
 	k3d::user_property_changed_signal m_user_property_changed_signal;
 	
 };
@@ -188,5 +136,4 @@ k3d::iplugin_factory& morph_points_factory()
 } // namespace deformation
 
 } // namespace module
-
 
--- tests/mesh.modifier.MorphPoints.py
+++ tests/mesh.modifier.MorphPoints.py
@@ -5,26 +5,27 @@ import testing
 
 document = k3d.new_document()
 
-source = document.new_node("PolyCone")
+source1 = document.new_node("PolyCone")
 source2 = document.new_node("PolySphere")
 source3 = document.new_node("PolyCube")
 modifier = document.new_node("MorphPoints")
 
-modifier.create_property("k3d::mesh*", "input_mesh2", "input_mesh2", "input_mesh2")
-modifier.create_property("k3d::double_t", "input_mesh2_percent", "input_mesh2_percent", "input_mesh2_percent")
+modifier.create_property("k3d::mesh*", "input1", "input1", "input1")
+modifier.create_property("k3d::mesh*", "input2", "input2", "input2")
+modifier.create_property("k3d::double_t", "amount1", "amount1", "amount1")
+modifier.create_property("k3d::double_t", "amount2", "amount2", "amount2")
 
-document.set_dependency(modifier.get_property("input_mesh"), source.get_property("output_mesh"))
-document.set_dependency(modifier.get_property("input_mesh1"), source2.get_property("output_mesh"))
-document.set_dependency(modifier.get_property("input_mesh2"), source3.get_property("output_mesh"))
+document.set_dependency(modifier.get_property("input_mesh"), source1.get_property("output_mesh"))
+document.set_dependency(modifier.get_property("input1"), source2.get_property("output_mesh"))
+document.set_dependency(modifier.get_property("input2"), source3.get_property("output_mesh"))
 
 selection = k3d.deselect_all()
 selection.points = k3d.component_select_all()
 
 modifier.mesh_selection = selection
 
-modifier.input_mesh1_percent = 10.
-modifier.input_mesh2_percent = 10.
-
-#mesh = modifier.output_mesh
+modifier.amount1 = 0.1
+modifier.amount2 = 0.1 
 
 testing.mesh_comparison(document, modifier.get_property("output_mesh"), "mesh.modifier.MorphPoints", 1)
+
