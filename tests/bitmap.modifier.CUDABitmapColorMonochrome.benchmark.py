9091474a4da2b6657e61eebac6b4ac5df84a2391
Evan Lezar <evanlezar@gmail.com>
2008-05-22 15:23:04 +0000

    COMP:  Removed BitmapColorMonochrome runs from CUDABitmapColorMonochrome benchmark

--- tests/bitmap.modifier.CUDABitmapColorMonochrome.benchmark.py
+++ tests/bitmap.modifier.CUDABitmapColorMonochrome.benchmark.py
@@ -3,17 +3,13 @@
 import testing
 import k3d
 
-# benchmark the CUDABitmapColorMonochrome plugin as well as the BitmapColorMonochrome plugin for comparative purposes
+# benchmark the CUDABitmapColorMonochrome plugin for comparative purposes
 testing.bitmap_benchmark("CUDABitmapColorMonochrome", (640,480), 10)
-testing.bitmap_benchmark("BitmapColorMonochrome", (640,480), 10)
 
 testing.bitmap_benchmark("CUDABitmapColorMonochrome", (800,600), 10)
-testing.bitmap_benchmark("BitmapColorMonochrome", (800,600), 10)
 
 testing.bitmap_benchmark("CUDABitmapColorMonochrome", (1024,768), 10)
-testing.bitmap_benchmark("BitmapColorMonochrome", (1024,768), 10)
 
 testing.bitmap_benchmark("CUDABitmapColorMonochrome", (1280,1024), 10)
-testing.bitmap_benchmark("BitmapColorMonochrome", (1280,1024), 10)
 
 
