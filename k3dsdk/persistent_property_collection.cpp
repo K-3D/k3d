63474254d8ecdb0ac0d7d5f05c593cca88aa763f
Timothy M. Shead <tshead@k-3d.com>
2008-06-01 19:53:25 +0000

    ENH: implement serialization for property metadata.

--- k3dsdk/persistent_property_collection.cpp
+++ k3dsdk/persistent_property_collection.cpp
@@ -21,6 +21,7 @@
 	\author Tim Shead (tshead@k-3d.com)
 */
 
+#include "imetadata.h"
 #include "inode.h"
 #include "iproperty_collection.h"
 #include "iuser_property.h"
@@ -33,7 +34,7 @@
 #include "xml.h"
 
 #include <iostream>
-#include <map>
+#include <set>
 
 namespace k3d
 {
@@ -96,111 +97,172 @@ void persistent_property_collection::save(xml::element& Element, const ipersiste
 	if(m_properties.empty())
 		return;
 
-	// Create an XML element to contain our proxies if it doesn't already exist ...
-	xml::element& xml_properties = Element.safe_element("properties");
+	// Save property values ...
+	xml::element& xml_properties = Element.append(xml::element("properties"));
+	for(uint_t i = 0; i != m_names.size(); ++i)
+		(*m_properties[i]).save(xml_properties, Context);
+	
+	// Save property metadata ...
+	/** \todo Next time we break backwards compatibility on documents, merge properties and their metadata */
+	for(uint_t i = 0; i != m_names.size(); ++i)
+	{
+		if(imetadata* const metadata = dynamic_cast<imetadata*>(m_properties[i]))
+		{
+			xml::element& xml_metadata = xml_properties.append(xml::element("metadata", xml::attribute("name", m_names[i])));
 
-	// Save 'em ...
-	for(properties_t::iterator property = m_properties.begin(); property != m_properties.end(); ++property)
-		(**property).save(xml_properties, Context);
+			const imetadata::metadata_t& pairs = metadata->get_metadata();
+			for(imetadata::metadata_t::const_iterator pair = pairs.begin(); pair != pairs.end(); ++pair)
+				xml_metadata.append(xml::element("pair", xml::attribute("name", pair->first), xml::attribute("value", pair->second)));
+		}
+	}
 }
 
 void persistent_property_collection::load(xml::element& Element, const ipersistent::load_context& Context)
 {
-	// Create an order-preserving mapping from names to XML elements ...
-	std::vector<string_t> xml_names;
-	std::vector<xml::element*> xml_property_storage;
-
 	xml::element* const xml_properties = xml::find_element(Element, "properties");
 	if(!xml_properties)
 		return;
 
+	// Keep-track of whether this object is a document node (required for user-properties) ...
+	k3d::inode* const node = dynamic_cast<k3d::inode*>(this);
+
+	// Keep-track of xml property names as we go, for error-detection ...
+	std::set<string_t> xml_property_names;
+
+	// Load <property> tags ...
 	for(xml::element::elements_t::iterator xml_property = xml_properties->children.begin(); xml_property != xml_properties->children.end(); ++xml_property)
 	{
 		if(xml_property->name != "property")
 			continue;
 
-		const string_t name = xml::attribute_text(*xml_property, "name");
-		if(name.empty())
-			continue;
-
-		if(std::count(xml_names.begin(), xml_names.end(), name))
+		const string_t xml_property_name = xml::attribute_text(*xml_property, "name");
+		if(xml_property_name.empty())
 		{
-			log() << warning << "Duplicate property [" << name << "] will be ignored" << std::endl;
+			log() << error << "property with missing name attribute will not be loaded" << std::endl;
 			continue;
 		}
 
-		xml_names.push_back(name);
-		xml_property_storage.push_back(&(*xml_property));
-	}
-
-	// Match builtin properties to each XML element ...
-	for(uint_t i = 0; i != m_names.size(); ++i)
-	{
-		for(uint_t j = 0; j != xml_names.size(); ++j)
+		if(xml_property_names.count(xml_property_name))
 		{
-			if(xml_names[j] == m_names[i])
+			log() << error << "property with duplicate name [" << xml_property_name << "] will not be loaded" << std::endl;
+			continue;
+		}
+
+		xml_property_names.insert(xml_property_name);
+
+		bool xml_property_used = false;
+
+		// Try matching the XML property to a builtin property ...
+		if(!xml_property_used)
+		{
+			for(uint_t i = 0; i != m_names.size(); ++i)
 			{
-				m_properties[i]->load(*xml_property_storage[j], Context);
-				xml_names.erase(xml_names.begin() + j);
-				xml_property_storage.erase(xml_property_storage.begin() + j);
-				break;
+				if(xml_property_name == m_names[i])
+				{
+					m_properties[i]->load(*xml_property, Context);
+					xml_property_used = true;
+					break;
+				}
 			}
 		}
-	}
 
-	// If we don't have anything else to look at, we're done ...
-	if(xml_property_storage.empty())
-		return;
-
-	// Otherwise, everything left-over should be a user-property ...
-	if(k3d::inode* const node = dynamic_cast<k3d::inode*>(this))
-	{
-		for(uint_t j = 0; j != xml_property_storage.size(); ++j)
+		// See if the XML property is a user-property to be instantiated ...
+		if(!xml_property_used && node)
 		{
-			xml::element& xml_property = *xml_property_storage[j];
-
-			const string_t user_property = xml::attribute_text(xml_property, "user_property");
-			if(user_property == "generic" || user_property == "vanilla" /* for backwards compatibility */)
+			const string_t xml_user_property = xml::attribute_text(*xml_property, "user_property");
+			if(xml_user_property == "generic" || xml_user_property == "vanilla" /* for backwards compatibility */)
 			{
-				const string_t type = xml::attribute_text(xml_property, "type");
-				const string_t name = xml::attribute_text(xml_property, "name");
-				const string_t label = xml::attribute_text(xml_property, "label");
-				const string_t description = xml::attribute_text(xml_property, "description");
+				xml_property_used = true;
+
+				const string_t type = xml::attribute_text(*xml_property, "type");
+				const string_t name = xml::attribute_text(*xml_property, "name");
+				const string_t label = xml::attribute_text(*xml_property, "label");
+				const string_t description = xml::attribute_text(*xml_property, "description");
 
 				k3d::iproperty* const new_property = property::create(*node, type, name, label, description);
-				if(!new_property)
+
+				if(new_property)
+				{
+					if(k3d::ipersistent* const persistent = dynamic_cast<k3d::ipersistent*>(new_property))
+						persistent->load(*xml_property, Context);
+				}
+				else
 				{
 					log() << error << k3d_file_reference << ": error creating property [" << name << "] with type [" << type << "]" << std::endl;
-					continue;
 				}
-
-				if(k3d::ipersistent* const persistent = dynamic_cast<k3d::ipersistent*>(new_property))
-					persistent->load(xml_property, Context);
 			}
-			else if(user_property == "renderman")
+			else if(xml_user_property == "renderman")
 			{
-				const string_t type = xml::attribute_text(xml_property, "type");
-				const string_t parameter_type = xml::attribute_text(xml_property, "parameter_type");
-				const string_t parameter_name = xml::attribute_text(xml_property, "parameter_name");
-				const string_t name = xml::attribute_text(xml_property, "name");
-				const string_t label = xml::attribute_text(xml_property, "label");
-				const string_t description = xml::attribute_text(xml_property, "description");
+				xml_property_used = true;
+
+				const string_t type = xml::attribute_text(*xml_property, "type");
+				const string_t parameter_type = xml::attribute_text(*xml_property, "parameter_type");
+				const string_t parameter_name = xml::attribute_text(*xml_property, "parameter_name");
+				const string_t name = xml::attribute_text(*xml_property, "name");
+				const string_t label = xml::attribute_text(*xml_property, "label");
+				const string_t description = xml::attribute_text(*xml_property, "description");
 
 				k3d::iproperty* new_property = 0;
 				if(parameter_type == "attribute")
 					new_property = property::ri::create_attribute(*node, type, parameter_name, name, label, description);
 				else if(parameter_type == "option")
 					new_property = property::ri::create_option(*node, type, parameter_name, name, label, description);
-					
-				if(!new_property)
+			
+				if(new_property)
+				{
+					if(k3d::ipersistent* const persistent = dynamic_cast<k3d::ipersistent*>(new_property))
+						persistent->load(*xml_property, Context);
+				}
+				else
 				{
 					log() << error << k3d_file_reference << ": error creating property [" << name << "] with type [" << type << "]" << std::endl;
-					continue;
 				}
-
-				if(k3d::ipersistent* const persistent = dynamic_cast<k3d::ipersistent*>(new_property))
-					persistent->load(xml_property, Context);
 			}
+			else if(xml_user_property.size())
+			{
+				log() << error << "unknown user property type: " << xml_user_property << std::endl;
+			}
+		}
+	}
+
+	// Load <metadata> tags ...
+	for(xml::element::elements_t::iterator xml_metadata = xml_properties->children.begin(); xml_metadata != xml_properties->children.end(); ++xml_metadata)
+	{
+		if(xml_metadata->name != "metadata")
+			continue;
+
+		const string_t xml_metadata_name = xml::attribute_text(*xml_metadata, "name");
+		if(xml_metadata_name.empty())
+		{
+			log() << error << "property metadata with missing name attribute will not be loaded" << std::endl;
+			continue;
+		}
+
+		if(!xml_property_names.count(xml_metadata_name))
+		{
+			log() << error << "property metadata with unknown name [" << xml_metadata_name << "] will not be loaded" << std::endl;
+			continue;
+		}
+
+		// Try matching the XML metadata to a property ...
+		for(uint_t i = 0; i != m_names.size(); ++i)
+		{
+			if(xml_metadata_name != m_names[i])
+				continue;
+
+			imetadata::metadata_t pairs;
+			for(xml::element::elements_t::const_iterator xml_pair = xml_metadata->children.begin(); xml_pair != xml_metadata->children.end(); ++xml_pair)
+			{
+				if(xml_pair->name != "pair")
+					continue;
+
+				pairs.insert(std::make_pair(xml::attribute_text(*xml_pair, "name"), xml::attribute_text(*xml_pair, "value")));
+			}
+
+			if(imetadata* const metadata = dynamic_cast<imetadata*>(m_properties[i]))
+				metadata->set_metadata(pairs);
+
+			break;
 		}
 	}
 }
