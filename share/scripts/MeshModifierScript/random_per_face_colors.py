8a2e32cb514ef0f455389a92d6c526e4a081293f
Timothy M. Shead <tshead@k-3d.com>
2008-07-05 16:10:45 +0000

    BUG: Fix script syntax error, and generate a better collection of random colors.

--- share/scripts/MeshModifierScript/random_per_face_colors.py
+++ share/scripts/MeshModifierScript/random_per_face_colors.py
@@ -1,30 +1,33 @@
 #python
 
 import k3d
-k3d.check_node_environment(locals(), "MeshModifierScript"
+k3d.check_node_environment(locals(), "MeshModifierScript")
 
-from random import seed, uniform
+from random import seed, randint
 
 Output.copy(Input)
 
 seed(123)
 
+colors = [ k3d.color(1, 0, 0), k3d.color(1, 1, 0), k3d.color(0, 1, 0), k3d.color(0, 1, 1), k3d.color(0, 0, 1), k3d.color(1, 0, 1), k3d.color(1, 1, 1)]
+
 if Output.bilinear_patches() and Output.bilinear_patches().patch_points():
 	Cs = Output.writable_bilinear_patches().writable_uniform_data().create_array("Cs", "k3d::color")
 	for i in range(len(Output.bilinear_patches().patch_points()) / 4):
-		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
+		Cs.append(colors[i % len(colors)])
 
 if Output.bicubic_patches() and Output.bicubic_patches().patch_points():
 	Cs = Output.writable_bicubic_patches().writable_uniform_data().create_array("Cs", "k3d::color")
 	for i in range(len(Output.bicubic_patches().patch_points()) / 16):
-		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
+		Cs.append(colors[i % len(colors)])
 
 if Output.nurbs_patches() and Output.nurbs_patches().patch_first_points():
 	Cs = Output.writable_nurbs_patches().writable_uniform_data().create_array("Cs", "k3d::color")
 	for i in range(len(Output.nurbs_patches().patch_first_points())):
-		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
+		Cs.append(colors[i % len(colors)])
 
 if Output.polyhedra() and Output.polyhedra().face_first_loops():
 	Cs = Output.writable_polyhedra().writable_uniform_data().create_array("Cs", "k3d::color")
 	for i in range(len(Output.polyhedra().face_first_loops())):
-		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
+		Cs.append(colors[i % len(colors)])
+
