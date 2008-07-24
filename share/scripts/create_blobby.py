e411cd4aef3e4af9de6ec0ee2d4b93a41eb72b88
Timothy M. Shead <tshead@k-3d.com>
2008-07-24 04:51:55 +0000

    ENH: Make our sample blobby-creation script more consistent with other examples.

--- share/scripts/create_blobby.py
+++ share/scripts/create_blobby.py
@@ -10,29 +10,29 @@ try:
 
 	mesh = k3d.dynamic_cast(frozen_mesh, "imesh_storage").reset_mesh()
 
-	ellipsoids = [k3d.point3(-1, 0, 1), k3d.point3(1, 0, 1), k3d.point3(1, 0, -1), k3d.point3(-1, 0, -1)]
-
 	blobbies = mesh.create_blobbies()
-
 	first_primitives = blobbies.create_first_primitives()
-	first_primitives.append(0)
-
 	primitive_counts = blobbies.create_primitive_counts()
-	primitive_counts.append(len(ellipsoids) + 1)
-
 	first_operators = blobbies.create_first_operators()
-	first_operators.append(0)
-
 	operator_counts = blobbies.create_operator_counts()
-	operator_counts.append(1)
-
 	materials = blobbies.create_materials()
-	materials.append(k3d.dynamic_cast(Document.get_node("Material"), "imaterial"))
-
 	primitives = blobbies.create_primitives()
 	primitive_first_floats = blobbies.create_primitive_first_floats()
 	primitive_float_counts = blobbies.create_primitive_float_counts()
 	floats = blobbies.create_floats()
+	Cs = blobbies.writable_varying_data().create_array("Cs", "k3d::color")
+	operators = blobbies.create_operators()
+	operator_first_operands = blobbies.create_operator_first_operands()
+	operator_operand_counts = blobbies.create_operator_operand_counts()
+	operands = blobbies.create_operands()
+
+	ellipsoids = [k3d.point3(-1, 0, 1), k3d.point3(1, 0, 1), k3d.point3(1, 0, -1), k3d.point3(-1, 0, -1)]
+
+	first_primitives.append(len(primitives))
+	primitive_counts.append(len(ellipsoids) + 1)
+	first_operators.append(len(operators))
+	operator_counts.append(1)
+	materials.append(k3d.dynamic_cast(Document.get_node("Material"), "imaterial"))
 
 	for center in ellipsoids:
 		primitives.append(k3d.primitive_type.ellipsoid)
@@ -54,19 +54,12 @@ try:
 	for i in k3d.identity3().column_major_list():
 		floats.append(i)
 
-	Cs = blobbies.writable_varying_data().create_array("Cs", "k3d::color")
 	Cs.assign([k3d.color(1, 0, 0), k3d.color(0, 1, 0), k3d.color(0, 0, 1), k3d.color(1, 1, 0), k3d.color(1, 1, 1)])
-
-	operators = blobbies.create_operators()
-	operator_first_operands = blobbies.create_operator_first_operands()
-	operator_operand_counts = blobbies.create_operator_operand_counts()
-	operands = blobbies.create_operands()
 
 	operators.append(k3d.operator_type.add)
 	operator_first_operands.append(len(operands))
 	operator_operand_counts.append(6)
 	operands.assign([5, 0, 1, 2, 3, 4])
-
 
 	mesh_instance = doc.new_node("MeshInstance")
 	mesh_instance.name = "Blobby Instance"
