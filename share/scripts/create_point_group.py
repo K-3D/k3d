116fa53285cb5a1d275c2204d5ec03cbc54ca624
Timothy M. Shead <tshead@k-3d.com>
2008-07-26 04:12:57 +0000

    ENH: Update the point group sample script to produce vertex colors instead of varying colors, so it works intuitively with PointsToBlobby.

--- share/scripts/create_point_group.py
+++ share/scripts/create_point_group.py
@@ -15,6 +15,7 @@ try:
 	size = 10
 
 	points = mesh.create_points()
+	Cs = mesh.writable_vertex_data().create_array("Cs", "k3d::color")
 	point_selection = mesh.create_point_selection()
 	point_groups = mesh.create_point_groups()
 	first_points = point_groups.create_first_points()
@@ -22,11 +23,11 @@ try:
 	materials = point_groups.create_materials()
 	constantwidth = point_groups.writable_constant_data().create_array("constantwidth", "k3d::double_t")
 	group_points = point_groups.create_points()
-	Cs = point_groups.writable_varying_data().create_array("Cs", "k3d::color")
 
 	for i in range(count):
 		points.append(k3d.point3(uniform(-size, size), uniform(-size, size), uniform(-size, size)))
 		point_selection.append(0.0)
+		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
 
 	first_points.append(len(group_points))
 	point_counts.append(len(points))
@@ -36,8 +37,6 @@ try:
 	for i in range(len(points)):
 		group_points.append(i)
 
-	for i in range(len(points)):
-		Cs.append(k3d.color(uniform(0, 1), uniform(0, 1), uniform(0, 1)))
 	
 	mesh_instance = doc.new_node("MeshInstance")
 	mesh_instance.name = "Point Group Instance"
