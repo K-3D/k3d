5c15d9b226fe9bf026e72f6dc3d2f9e4921bd7a1
Carsten Haubold <CarstenHaubold@web.de>
2008-07-31 19:50:27 +0000

    ENH: Tests for CreateCap and CurveTraversal

---
+++ tests/mesh.modifier.NurbsCurveTraversal1.py
@@ -0,0 +1,16 @@
+#python
+
+import testing
+import k3d
+
+document = k3d.new_document()
+setup = testing.setup_mesh_reader_test("K3DMeshReader","mesh.modifier.NurbsCurveTraversalReference.k3d")
+
+
+modifier = setup.document.new_node("NurbsCurveTraversal")
+modifier.create_caps = False
+
+document.set_dependency(modifier.get_property("input_mesh"), setup.reader.get_property("output_mesh"))
+
+testing.mesh_comparison_to_reference(document, modifier.get_property("output_mesh"), "mesh.modifier.NurbsCurveTraversal1", 1)
+
