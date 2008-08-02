8a93a73793a3b2738fd9c1972efc7d094d1f555a
Bart Janssens <bart.janssens@lid.kviv.be>
2008-08-02 23:16:53 +0000

    BUG: Proposed fix for render Win32 crash

--- modules/ngui_material_manager_panel/RenderedImage.cpp
+++ modules/ngui_material_manager_panel/RenderedImage.cpp
@@ -38,26 +38,29 @@ RenderedImage::~RenderedImage()
 
 bool RenderedImage::on_expose_event(GdkEventExpose* event)
 {
-  try
-    {
-      Glib::RefPtr<Gdk::Pixbuf> image = Gdk::Pixbuf::create_from_file(m_imgFilePath.native_filesystem_string());
-      image->render_to_drawable(get_window(), get_style()->get_black_gc(),
-                                0, 0, 10, 10, image->get_width(), image->get_height(),
-                                Gdk::RGB_DITHER_NONE, 0, 0);
-    }
-  catch(Glib::FileError)
-    {
-      //No Image File. Display Default Image Holder
-      Glib::RefPtr<Gdk::Pixbuf> image = Gdk::Pixbuf::create_from_file(m_imgHolderPath.native_filesystem_string());
-      image->render_to_drawable(get_window(), get_style()->get_black_gc(),
-                                0, 0, 10, 10, image->get_width(), image->get_height(),
-                                Gdk::RGB_DITHER_NONE, 0, 0);
-    }
-  catch(Glib::Error)
-    {
+	try
+	{
+		if(k3d::filesystem::exists(m_imgFilePath))
+		{
+				Glib::RefPtr<Gdk::Pixbuf> image = Gdk::Pixbuf::create_from_file(m_imgFilePath.native_filesystem_string());
+				image->render_to_drawable(get_window(), get_style()->get_black_gc(),
+																	0, 0, 10, 10, image->get_width(), image->get_height(),
+																	Gdk::RGB_DITHER_NONE, 0, 0);
+		}
+		else
+		{
+			//No Image File. Display Default Image Holder
+			Glib::RefPtr<Gdk::Pixbuf> image = Gdk::Pixbuf::create_from_file(m_imgHolderPath.native_filesystem_string());
+			image->render_to_drawable(get_window(), get_style()->get_black_gc(),
+																0, 0, 10, 10, image->get_width(), image->get_height(),
+																Gdk::RGB_DITHER_NONE, 0, 0);
+		}
+	}
+  catch(...)
+	{
 
-      //Should not hopefully Get Here!
-    }
+		//Should not hopefully Get Here!
+	}
 
   return true;
 
