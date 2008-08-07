56c669c254b1e8bd8fc5330c708618fc99cf568f
Alex Curtis <alx.curtis@gmail.com>
2008-08-07 12:46:18 +0000

    BUG: Essential File Added

---
+++ modules/ngui_material_manager_panel/PreviewObj.cpp
@@ -0,0 +1,40 @@
+#include "PreviewObj.h"
+
+namespace module
+{
+namespace ngui
+{
+namespace material_manager
+{
+namespace mechanics
+{
+
+//Static Definitions
+const k3d::string_t PreviewObj::pview_geo_nametag_mt
+= "PreviewCore::nametag";
+
+const k3d::string_t PreviewObj::sphere_md
+= "p_sphere_geo";
+
+const k3d::string_t PreviewObj::cube_md
+= "p_cube_geo";
+
+const k3d::string_t PreviewObj::torus_md
+= "p_torus_geo";
+
+const k3d::string_t PreviewObj::sphere_node_name
+= "Preview Core::Geo::Sphere";
+
+const k3d::string_t PreviewObj::cube_node_name 
+= "Preview Core::Geo::Cube";
+
+const k3d::string_t PreviewObj::torus_node_name
+= "Preview Core::Geo::Torus";
+
+}//namespace mechanics
+
+}//namespace material_manager
+
+}//namespace ngui
+
+}//namespace module
