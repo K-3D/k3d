2ca845b8742bc633ab4d6aa08827eeeda0f847bb
Carsten Haubold <CarstenHaubold@web.de>
2008-08-11 14:32:14 +0000

    ENH: more NURBS tests

---
+++ tests/mesh.modifier.NurbsSetWeight1.py
@@ -0,0 +1,15 @@
+#python
+
+import testing
+import k3d
+
+document = k3d.new_document()
+setup = testing.setup_mesh_reader_test("K3DMeshReader","mesh.modifier.NurbsSetWeightReference1.k3d")
+
+modifier = setup.document.new_node("NurbsSetWeight")
+modifier.weight = 1.7
+
+document.set_dependency(modifier.get_property("input_mesh"), setup.reader.get_property("output_mesh"))
+
+testing.mesh_comparison_to_reference(document, modifier.get_property("output_mesh"), "mesh.modifier.NurbsSetWeight1", 1)
+
