a20f0d5222b088e8dc9483f315410f387f3576ba
Timothy M. Shead <tshead@k-3d.com>
2008-08-26 00:45:58 +0000

    COMP: Require CMake 2.6.1, and explicitly set the old policy for setting link-library paths.

--- tests/test.measurements.py
+++ tests/test.measurements.py
@@ -1,13 +1,46 @@
 #python
 
+import sys
 import testing
 
-print """<DartMeasurement name="Test_Float" type="numeric/float">0.12</DartMeasurement>"""
-print """<DartMeasurement name="Test_Integer" type="numeric/integer">12</DartMeasurement>"""
-print """<DartMeasurement name="Test_String" type="text/string">A simple message</DartMeasurement>"""
-print """<DartMeasurement name="Test_Text" type="text/text">A longer message</DartMeasurement>"""
-print """<DartMeasurement name="Test_HTML" type="text/html"><![CDATA[<table><tr><th>foo</th><th>bar</th></tr><tr><td>a</td><td>b</td></tr></table>]]></DartMeasurement>"""
-print """<DartMeasurement name="Test_XML" type="text/xml"><![CDATA[<foo><bar a="b"><baz c="d"/></bar></foo>]]></DartMeasurement>"""
-print """<DartMeasurementFile name="Test_JPEG" type="image/jpeg">""" + testing.source_path() + """/bitmaps/test_rgb_8.jpg</DartMeasurementFile>"""
-print """<DartMeasurementFile name="Test_PNG" type="image/png">""" + testing.source_path() + """/bitmaps/test_rgb_8.png</DartMeasurementFile>"""
+# Floating-point measurement
+print """<DartMeasurement name="Float" type="numeric/float">0.12</DartMeasurement>"""
+
+# Integer measurement
+print """<DartMeasurement name="Integer" type="numeric/integer">12</DartMeasurement>"""
+
+# String measurement
+print """<DartMeasurement name="String" type="text/string">A simple message</DartMeasurement>"""
+
+# Longer text measurement
+print """<DartMeasurement name="Text" type="text/text">A longer message</DartMeasurement>"""
+
+# An HTML table measurement
+print """<DartMeasurement name="HTML Table" type="text/html"><![CDATA[
+<table>
+<tr><th>Time</th><th>Value 1</th><th>Value 2</th></tr>
+<tr><td>1</td><td>1.2</td><td>1.6</td></tr>
+<tr><td>2</td><td>1.3</td><td>2.4</td></tr>
+<tr><td>3</td><td>1.4</td><td>4.5</td></tr>
+<tr><td>4</td><td>1.5</td><td>8.9</td></tr>
+</table>
+]]></DartMeasurement>"""
+
+# An HTML Google Chart measurement
+print """<DartMeasurement name="HTML Chart" type="text/html"><![CDATA[
+<img src="http://chart.apis.google.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World">
+]]></DartMeasurement>"""
+
+# An arbitrary XML measurement
+print """<DartMeasurement name="XML" type="text/xml"><![CDATA[
+<foo><bar a="b"><baz c="d"/></bar></foo>
+]]></DartMeasurement>"""
+
+# JPEG image measurement
+print """<DartMeasurementFile name="JPEG Image" type="image/jpeg">""" + testing.source_path() + """/bitmaps/test_rgb_8.jpg</DartMeasurementFile>"""
+
+# PNG image measurement
+print """<DartMeasurementFile name="PNG Image" type="image/png">""" + testing.source_path() + """/bitmaps/test_rgb_8.png</DartMeasurementFile>"""
+
+sys.stdout.flush()
 
