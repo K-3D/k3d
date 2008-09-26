89ca9452cf3a729132cd1227d74062fc5071323d
Timothy M. Shead <tshead@k-3d.com>
2008-09-26 21:29:08 +0000

    ENH: Remove some deprecated code.

--- share/scripts/MeshModifierScript/random_face_varying_colors.py
+++ share/scripts/MeshModifierScript/random_face_varying_colors.py
@@ -12,7 +12,7 @@ seed(123)
 colors = [ k3d.color(1, 0, 0), k3d.color(1, 1, 0), k3d.color(0, 1, 0), k3d.color(0, 1, 1), k3d.color(0, 0, 1), k3d.color(1, 0, 1), k3d.color(1, 1, 1)]
 
 if Output.polyhedra() and Output.polyhedra().edge_points():
-	Cs = Output.writable_polyhedra().writable_face_varying_data().create_array("Cs", "k3d::color")
+	Cs = Output.writable_polyhedra().writable_face_varying_data().create("Cs", "k3d::color")
 	for i in range(len(Output.polyhedra().edge_points())):
 		Cs.append(colors[i % len(colors)])
 
