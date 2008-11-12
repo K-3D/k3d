62af8ed4606b987c8276e941347750d9fd1dcaa0
Timothy M. Shead <tshead@k-3d.com>
2008-11-12 01:22:44 +0000

    ENH: Introduce some experimental new selection tokens for use with generic primitives.

--- k3dsdk/selection.h
+++ k3dsdk/selection.h
@@ -54,7 +54,7 @@ struct is_selected
 namespace selection
 {
 
-typedef enum
+enum type
 {
 
 /** \note Never alter the values of these enumerations, they may be used for efficient serialization */
@@ -65,52 +65,54 @@ NONE = 0,
 NODE = 1,
 /// Storage for a zero-based mesh index (relative to a node)
 MESH = 2,
-/// Storage for a zero-based point group index (relative to a mesh)
 //POINT_GROUP = 3,
-/// Storage for a zero-based point index (relative to a point group)
 //POINT = 4,
-/// Storage for a zero-based point index (relative to a mesh)
+/** \deprecated Storage for a zero-based point index (relative to a mesh) */
 ABSOLUTE_POINT = 5,
-/// Storage for a zero-based polyhedron index (relative to a mesh)
 //POLYHEDRON = 6,
-/// Storage for a zero-based face index (relative to a polyhedron)
 //FACE = 7,
-/// Storage for a zero-based face index (relative to a mesh)
+/** \deprecated Storage for a zero-based face index (relative to a mesh) */
 ABSOLUTE_FACE = 8,
-/// Storage for a zero-based face hole index (relative to a face)
 //FACE_HOLE = 9,
-/// Storage for a zero-based split-edge index (relative to a polyhedron)
 //SPLIT_EDGE = 10,
-/// Storage for a zero-based split-edge index (relative to a mesh)
+/** \deprecated Storage for a zero-based split-edge index (relative to a mesh) */
 ABSOLUTE_SPLIT_EDGE = 11,
-/// Storage for a zero-based linear curve group index (relative to a mesh)
 //LINEAR_CURVE_GROUP = 12,
-/// Storage for a zero-based linear curve index (relative to a linear curve group)
 //LINEAR_CURVE = 13,
-/// Storage for a zero-based linear curve index (relative to a mesh)
+/** \deprecated Storage for a zero-based linear curve index (relative to a mesh) */
 ABSOLUTE_LINEAR_CURVE = 14,
-/// Storage for a zero-based cubic curve group index (relative to a mesh)
 //CUBIC_CURVE_GROUP = 15,
-/// Storage for a zero-based cubic curve index (relative to a cubic curve group)
 //CUBIC_CURVE = 16,
-/// Storage for a zero-based cubic curve group index (relative to a mesh)
+/** \deprecated Storage for a zero-based cubic curve group index (relative to a mesh) */
 ABSOLUTE_CUBIC_CURVE = 17,
-/// Storage for a zero-based NURBS curve group index (relative to a mesh)
 //NUCURVE_GROUP = 18,
-/// Storage for a zero-based NURBS curve index (relative to a NURBS curve group)
 //NUCURVE = 19,
-/// Storage for a zero-based NURBS curve index (relative to a mesh)
+/** \deprecated Storage for a zero-based NURBS curve index (relative to a mesh) */
 ABSOLUTE_NURBS_CURVE = 20,
-/// Storage for a zero-based bilinear patch index (relative to a mesh)
+/** \deprecated Storage for a zero-based bilinear patch index (relative to a mesh) */
 ABSOLUTE_BILINEAR_PATCH = 21,
-/// Storage for a zero-based bicubic patch index (relative to a mesh)
+/** \deprecated Storage for a zero-based bicubic patch index (relative to a mesh) */
 ABSOLUTE_BICUBIC_PATCH = 22,
-/// Storage for a zero-based NURBS patch index (relative to a mesh)
+/** \deprecated Storage for a zero-based NURBS patch index (relative to a mesh) */
 ABSOLUTE_NURBS_PATCH = 23,
 /// Storage for a user-defined index
-USER1 = 24
+USER1 = 24,
+/// Storage for a zero-based primitive index (relative to a mesh)
+PRIMITIVE = 25,
+/// Storage for a zero-based constant index (relative to a primitive)
+CONSTANT = 26,
+/// Storage for a zero-based uniform index (relative to a primitive)
+UNIFORM = 27,
+/// Storage for a zero-based varying index (relative to a primitive)
+VARYING = 28,
+/// Storage for a zero-based face-varying index (relative to a primitive)
+FACE_VARYING = 29,
+/// Storage for a zero-based split-edge index (relative to a primitive)
+SPLIT_EDGE = 30,
+/// Storage for a zero-based vertex index (relative to a primitive)
+VERTEX = 31,
 
-} type;
+};
 
 std::ostream& operator<<(std::ostream& Stream, const type& RHS);
 std::istream& operator>>(std::istream& Stream, type& RHS);
