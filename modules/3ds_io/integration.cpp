76536e900d9cfd2942d50e36347c5ab14fbccd09
Timothy M. Shead <tshead@k-3d.com>
2009-02-05 07:37:58 +0000

    ENH: Prepare 3DSMeshReader for the transition to generic primitives.

--- modules/3ds_io/integration.cpp
+++ modules/3ds_io/integration.cpp
@@ -1,6 +1,7 @@
 #include "integration.h"
 
 #include <k3dsdk/algebra.h>
+#include <k3dsdk/polyhedron.h>
 
 #include <lib3ds/file.h>
 #include <lib3ds/camera.h>
@@ -11,6 +12,8 @@
 #include <lib3ds/vector.h>
 #include <lib3ds/light.h>
 
+#include <boost/scoped_ptr.hpp>
+
 namespace module
 {
 
@@ -20,12 +23,12 @@ namespace f3ds
 namespace io
 {
 
-static void add_nodes(k3d::mesh& Mesh, Lib3dsFile* file, Lib3dsNode* node, k3d::uint_t &offset)
+static void add_nodes(k3d::mesh& Mesh, Lib3dsFile* file, Lib3dsNode* node, k3d::uint_t &offset, k3d::polyhedron::primitive& polyhedron)
 {
 	if(node->type == LIB3DS_OBJECT_NODE)
 	{
 		for(Lib3dsNode* p = node->childs; p != 0; p = p->next)
-			add_nodes(Mesh, file, p, offset);
+			add_nodes(Mesh, file, p, offset, polyhedron);
 
 		if(strcmp(node->name,"$$$DUMMY") == 0)
 			return;
@@ -37,35 +40,25 @@ static void add_nodes(k3d::mesh& Mesh, Lib3dsFile* file, Lib3dsNode* node, k3d::
 		if(!mesh)
 			return;
 
-		k3d::mesh::polyhedra_t& polyhedra = Mesh.polyhedra.writable();
-		k3d::mesh::indices_t& first_faces = polyhedra.first_faces.writable();
-		k3d::mesh::counts_t& face_counts = polyhedra.face_counts.writable();
-		k3d::mesh::polyhedra_t::types_t& types = polyhedra.types.writable();
-		k3d::mesh::indices_t& face_first_loops = polyhedra.face_first_loops.writable();
-		k3d::mesh::counts_t& face_loop_counts = polyhedra.face_loop_counts.writable();
-		k3d::mesh::selection_t& face_selection = polyhedra.face_selection.writable();
-		k3d::mesh::materials_t& face_materials = polyhedra.face_materials.writable();
-		k3d::mesh::indices_t& loop_first_edges = polyhedra.loop_first_edges.writable();
-		k3d::mesh::indices_t& edge_points = polyhedra.edge_points.writable();
-		k3d::mesh::indices_t& clockwise_edges = polyhedra.clockwise_edges.writable();
-		k3d::mesh::selection_t& edge_selection = polyhedra.edge_selection.writable();
 		for(int p = 0; p < mesh->faces; ++p)
 		{
 			Lib3dsFace* const f = &mesh->faceL[p];
 
-			face_first_loops.push_back(loop_first_edges.size());
-			face_loop_counts.push_back(1);
-			face_selection.push_back(0);
-			face_materials.push_back(0);
-			loop_first_edges.push_back(edge_points.size());
+			polyhedron.face_counts.back() = polyhedron.face_counts.back() + 1;
+
+			polyhedron.face_first_loops.push_back(polyhedron.loop_first_edges.size());
+			polyhedron.face_loop_counts.push_back(1);
+			polyhedron.face_selections.push_back(0);
+			polyhedron.face_materials.push_back(0);
+			polyhedron.loop_first_edges.push_back(polyhedron.edge_points.size());
 
 			for(int j = 0; j != 3; ++j)
 			{
-				edge_points.push_back(f->points[j] + offset);
-				clockwise_edges.push_back(edge_points.size());
-				edge_selection.push_back(0);
+				polyhedron.edge_points.push_back(f->points[j] + offset);
+				polyhedron.clockwise_edges.push_back(polyhedron.edge_points.size());
+				polyhedron.edge_selections.push_back(0);
 			}
-			clockwise_edges.back() = loop_first_edges.back();
+			polyhedron.clockwise_edges.back() = polyhedron.loop_first_edges.back();
 		}
 
 		k3d::mesh::points_t& points = Mesh.points.writable();
@@ -92,18 +85,10 @@ f3dsParser::f3dsParser(const char *filename, k3d::mesh &Mesh)
 	Mesh.points.create();
 	Mesh.point_selection.create();
 
-	k3d::mesh::polyhedra_t& polyhedra = Mesh.polyhedra.create();
-	k3d::mesh::indices_t& first_faces = polyhedra.first_faces.create();
-	k3d::mesh::counts_t& face_counts = polyhedra.face_counts.create();
-	k3d::mesh::polyhedra_t::types_t& types = polyhedra.types.create();
-	k3d::mesh::indices_t& face_first_loops = polyhedra.face_first_loops.create();
-	k3d::mesh::counts_t& face_loop_counts = polyhedra.face_loop_counts.create();
-	k3d::mesh::selection_t& face_selection = polyhedra.face_selection.create();
-	k3d::mesh::materials_t& face_materials = polyhedra.face_materials.create();
-	k3d::mesh::indices_t& loop_first_edges = polyhedra.loop_first_edges.create();
-	k3d::mesh::indices_t& edge_points = polyhedra.edge_points.create();
-	k3d::mesh::indices_t& clockwise_edges = polyhedra.clockwise_edges.create();
-	k3d::mesh::selection_t& edge_selection = polyhedra.edge_selection.create();
+	boost::scoped_ptr<k3d::polyhedron::primitive> polyhedron(k3d::polyhedron::create(Mesh));
+	polyhedron->first_faces.push_back(0);
+	polyhedron->face_counts.push_back(0);
+	polyhedron->polyhedron_types.push_back(k3d::mesh::polyhedra_t::POLYGONS);
 
 	/* No nodes?  Fabricate nodes to display all the meshes. */
 	if(!file->nodes)
@@ -121,14 +106,7 @@ f3dsParser::f3dsParser(const char *filename, k3d::mesh &Mesh)
 
 	k3d::uint_t offset = 0;
 	for(Lib3dsNode* p = file->nodes; p; p = p->next)
-		add_nodes(Mesh, file, p, offset);
-
-	if(face_first_loops.size())
-	{
-		first_faces.push_back(0);
-		face_counts.push_back(face_first_loops.size());
-		types.push_back(k3d::mesh::polyhedra_t::POLYGONS);
-	}
+		add_nodes(Mesh, file, p, offset, *polyhedron);
 }
 
 } // namespace io
