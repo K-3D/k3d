3733793f6d9771fc67b128793e85f82baab023a0
Timothy M. Shead <tshead@k-3d.com>
2009-02-19 01:55:40 +0000

    ENH: Convert NurbsHyperboloid to generic primitives.

--- modules/nurbs/hyperboloid.cpp
+++ modules/nurbs/hyperboloid.cpp
@@ -18,17 +18,20 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /** \file
-		\author Tim Shead (tshead@k-3d.com)
+	\author Tim Shead (tshead@k-3d.com)
 */
 
 #include <k3d-i18n-config.h>
 #include <k3dsdk/document_plugin_factory.h>
 #include <k3dsdk/imaterial.h>
-#include <k3dsdk/legacy_mesh_source.h>
+#include <k3dsdk/mesh_source.h>
 #include <k3dsdk/material_sink.h>
 #include <k3dsdk/measurement.h>
 #include <k3dsdk/node.h>
 #include <k3dsdk/nurbs.h>
+#include <k3dsdk/nurbs_patch.h>
+
+#include <boost/scoped_ptr.hpp>
 
 namespace module
 {
@@ -40,84 +43,117 @@ namespace nurbs
 // hyperboloid
 
 class hyperboloid :
-			public k3d::material_sink<k3d::legacy::mesh_source<k3d::node > >
+	public k3d::material_sink<k3d::mesh_source<k3d::node > >
 {
-	typedef k3d::material_sink<k3d::legacy::mesh_source<k3d::node > > base;
+	typedef k3d::material_sink<k3d::mesh_source<k3d::node > > base;
 
 public:
 	hyperboloid(k3d::iplugin_factory& Factory, k3d::idocument& Document) :
-			base(Factory, Document),
-			m_x1(init_owner(*this) + init_name("x1") + init_label(_("x1")) + init_description(_("x1")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_y1(init_owner(*this) + init_name("y1") + init_label(_("y1")) + init_description(_("y1")) + init_value(-5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_z1(init_owner(*this) + init_name("z1") + init_label(_("z1")) + init_description(_("z1")) + init_value(-5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_x2(init_owner(*this) + init_name("x2") + init_label(_("x2")) + init_description(_("x2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_y2(init_owner(*this) + init_name("y2") + init_label(_("y2")) + init_description(_("y2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_z2(init_owner(*this) + init_name("z2") + init_label(_("z2")) + init_description(_("z2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
-			m_thetamax(init_owner(*this) + init_name("thetamax") + init_label(_("thetamax")) + init_description(_("thetamax")) + init_value(k3d::radians(360.0)) + init_step_increment(k3d::radians(1.0)) + init_units(typeid(k3d::measurement::angle)))
+		base(Factory, Document),
+		m_x1(init_owner(*this) + init_name("x1") + init_label(_("x1")) + init_description(_("x1")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_y1(init_owner(*this) + init_name("y1") + init_label(_("y1")) + init_description(_("y1")) + init_value(-5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_z1(init_owner(*this) + init_name("z1") + init_label(_("z1")) + init_description(_("z1")) + init_value(-5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_x2(init_owner(*this) + init_name("x2") + init_label(_("x2")) + init_description(_("x2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_y2(init_owner(*this) + init_name("y2") + init_label(_("y2")) + init_description(_("y2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_z2(init_owner(*this) + init_name("z2") + init_label(_("z2")) + init_description(_("z2")) + init_value(5.0) + init_step_increment(0.1) + init_units(typeid(k3d::measurement::distance))),
+		m_thetamax(init_owner(*this) + init_name("thetamax") + init_label(_("thetamax")) + init_description(_("thetamax")) + init_value(k3d::radians(360.0)) + init_step_increment(k3d::radians(1.0)) + init_units(typeid(k3d::measurement::angle)))
 	{
-		m_material.changed_signal().connect(make_reset_mesh_slot());
-		m_x1.changed_signal().connect(make_reset_mesh_slot());
-		m_y1.changed_signal().connect(make_reset_mesh_slot());
-		m_z1.changed_signal().connect(make_reset_mesh_slot());
-		m_x2.changed_signal().connect(make_reset_mesh_slot());
-		m_y2.changed_signal().connect(make_reset_mesh_slot());
-		m_z2.changed_signal().connect(make_reset_mesh_slot());
-		m_thetamax.changed_signal().connect(make_reset_mesh_slot());
+		m_material.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_x1.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_y1.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_z1.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_x2.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_y2.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_z2.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
+		m_thetamax.changed_signal().connect(k3d::hint::converter<
+			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_mesh_slot()));
 	}
 
-	void on_initialize_mesh(k3d::legacy::mesh& Mesh)
+	void on_update_mesh_topology(k3d::mesh& Output)
 	{
-		const double x1 = m_x1.pipeline_value();
-		const double y1 = m_y1.pipeline_value();
-		const double z1 = m_z1.pipeline_value();
-		const double x2 = m_x2.pipeline_value();
-		const double y2 = m_y2.pipeline_value();
-		const double z2 = m_z2.pipeline_value();
-		const double thetamax = m_thetamax.pipeline_value();
+		Output = k3d::mesh();
 
-		const double thetamin1 = atan2(y1, x1);
-		const double thetamin2 = atan2(y2, x2);
+		k3d::imaterial* const material = m_material.pipeline_value();
+		const k3d::double_t x1 = m_x1.pipeline_value();
+		const k3d::double_t y1 = m_y1.pipeline_value();
+		const k3d::double_t z1 = m_z1.pipeline_value();
+		const k3d::double_t x2 = m_x2.pipeline_value();
+		const k3d::double_t y2 = m_y2.pipeline_value();
+		const k3d::double_t z2 = m_z2.pipeline_value();
+		const k3d::double_t thetamax = m_thetamax.pipeline_value();
 
-		const double radius1 = sqrt(x1 * x1 + y1 * y1);
-		const double radius2 = sqrt(x2 * x2 + y2 * y2);
+		const k3d::double_t thetamin1 = atan2(y1, x1);
+		const k3d::double_t thetamin2 = atan2(y2, x2);
 
-		const k3d::point3 offset1 = z1 * k3d::point3(0, 0, 1);
-		const k3d::point3 offset2 = z2 * k3d::point3(0, 0, 1);
+		const k3d::double_t radius1 = sqrt(x1 * x1 + y1 * y1);
+		const k3d::double_t radius2 = sqrt(x2 * x2 + y2 * y2);
 
-		k3d::legacy::nupatch* const nupatch = new k3d::legacy::nupatch();
-		Mesh.nupatches.push_back(nupatch);
+		const k3d::vector3 offset1 = z1 * k3d::vector3(0, 0, 1);
+		const k3d::vector3 offset2 = z2 * k3d::vector3(0, 0, 1);
 
-		nupatch->material = m_material.pipeline_value();
-		nupatch->u_order = 3;
-		nupatch->v_order = 2;
+		// Compute NURBS control points ...
+		std::vector<k3d::double_t> knots1;
+		std::vector<k3d::double_t> weights1;
+		std::vector<k3d::point3> control_points1;
+		k3d::nurbs::circular_arc(k3d::vector3(1, 0, 0), k3d::vector3(0, 1, 0), thetamin1, thetamin1 + thetamax, 4, knots1, weights1, control_points1);
 
-		std::vector<double> weights1;
-		std::vector<k3d::point3> arc_points1;
-		k3d::nurbs::circular_arc(k3d::point3(1, 0, 0), k3d::point3(0, 1, 0), thetamin1, thetamin1 + thetamax, 4, nupatch->u_knots, weights1, arc_points1);
+		std::vector<k3d::double_t> knots2;
+		std::vector<k3d::double_t> weights2;
+		std::vector<k3d::point3> control_points2;
+		k3d::nurbs::circular_arc(k3d::vector3(1, 0, 0), k3d::vector3(0, 1, 0), thetamin2, thetamin2 + thetamax, 4, knots2, weights2, control_points2);
 
-		std::vector<double> weights2;
-		std::vector<k3d::point3> arc_points2;
-		k3d::nurbs::circular_arc(k3d::point3(1, 0, 0), k3d::point3(0, 1, 0), thetamin2, thetamin2 + thetamax, 4, nupatch->u_knots, weights2, arc_points2);
+		// Create patch ...
+		k3d::mesh::points_t& points = Output.points.create();
+		k3d::mesh::selection_t& point_selection = Output.point_selection.create();
+		boost::scoped_ptr<k3d::nurbs_patch::primitive> primitive(k3d::nurbs_patch::create(Output));
 
-		nupatch->v_knots.insert(nupatch->v_knots.end(), 2, 0);
-		nupatch->v_knots.insert(nupatch->v_knots.end(), 2, 1);
+		primitive->patch_first_points.push_back(primitive->patch_points.size());
+		primitive->patch_u_point_counts.push_back(control_points1.size());
+		primitive->patch_v_point_counts.push_back(2);
+		primitive->patch_u_orders.push_back(3);
+		primitive->patch_v_orders.push_back(2);
+		primitive->patch_u_first_knots.push_back(primitive->patch_u_knots.size());
+		primitive->patch_v_first_knots.push_back(primitive->patch_v_knots.size());
+		primitive->patch_selections.push_back(0);
+		primitive->patch_materials.push_back(material);
 
-		for (unsigned long j = 0; j != arc_points1.size(); ++j)
+		for(k3d::uint_t j = 0; j != control_points1.size(); ++j)
 		{
-			Mesh.points.push_back(new k3d::legacy::point(radius1 * arc_points1[j] + offset1));
-			nupatch->control_points.push_back(k3d::legacy::nupatch::control_point(Mesh.points.back(), weights1[j]));
+			primitive->patch_points.push_back(points.size());
+			primitive->patch_point_weights.push_back(weights1[j]);
+
+			points.push_back((radius1 * control_points1[j]) + offset1);
+			point_selection.push_back(0);
 		}
 
-		for (unsigned long j = 0; j != arc_points2.size(); ++j)
+		for(k3d::uint_t j = 0; j != control_points2.size(); ++j)
 		{
-			Mesh.points.push_back(new k3d::legacy::point(radius2 * arc_points2[j] + offset2));
-			nupatch->control_points.push_back(k3d::legacy::nupatch::control_point(Mesh.points.back(), weights2[j]));
+			primitive->patch_points.push_back(points.size());
+			primitive->patch_point_weights.push_back(weights2[j]);
+
+			points.push_back((radius2 * control_points2[j]) + offset2);
+			point_selection.push_back(0);
 		}
 
-		assert_warning(is_valid(*nupatch));
+		primitive->patch_u_knots.assign(knots1.begin(), knots1.end());
+
+		primitive->patch_v_knots.push_back(0);
+		primitive->patch_v_knots.push_back(0);
+		primitive->patch_v_knots.push_back(1);
+		primitive->patch_v_knots.push_back(1);
+
+		primitive->patch_trim_loop_counts.push_back(0);
+		primitive->patch_first_trim_loops.push_back(0);
 	}
 
-	void on_update_mesh(k3d::legacy::mesh& Mesh)
+	void on_update_mesh_geometry(k3d::mesh& Output)
 	{
 	}
 
@@ -134,13 +170,13 @@ public:
 	}
 
 private:
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_x1;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_y1;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_z1;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_x2;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_y2;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_z2;
-	k3d_data(double, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_thetamax;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_x1;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_y1;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_z1;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_x2;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_y2;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_z2;
+	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, measurement_property, with_serialization) m_thetamax;
 };
 
 /////////////////////////////////////////////////////////////////////////////
