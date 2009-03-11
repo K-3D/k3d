bf3ca99a3869ef91e99593f18a35c1fa1706d994
Timothy M. Shead <tshead@k-3d.com>
2009-03-11 04:49:24 +0000

    COMP: Fix broken regression test.

--- tests/python.api.angle_axis.py
+++ tests/python.api.angle_axis.py
@@ -3,11 +3,8 @@
 import k3d
 import math
 
-a = k3d.angle_axis(90, k3d.vector3(0, 0, 1))
+a = k3d.angle_axis(math.radians(90), k3d.vector3(0, 0, 1))
 
-if math.floor(a.angle * 10000) != 900000:
+if math.degrees(a.angle) != 90:
 	raise "angle incorrect"
-
-if math.floor(a.angle_radians * 10000) != 15707:
-	raise "internal angle incorrect"
 
