6cc31f1727159c21f4975e014ea60bc68345ceae
Timothy M. Shead <tshead@k-3d.com>
2009-04-01 04:25:53 +0000

    COMP: Remove obsolete test.

--- tests/mesh.source.SphereEversion.py
+++
@@ -1,8 +0,0 @@
-#python
-
-import testing
-
-setup = testing.setup_mesh_source_test("SphereEversion")
-setup.source.time = 0.25
-testing.mesh_comparison_to_reference(setup.document, setup.source.get_property("output_mesh"), "mesh.source.SphereEversion", 48)
-
