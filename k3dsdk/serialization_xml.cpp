6bd0c6050a416591af3ab3a6ff1b5ca142fd1ac7
Bart Janssens <bart.janssens@lid.kviv.be>
2009-04-10 11:18:18 +0000

    ENH: Add semi-functional legacy nurbs reading code to serialization_xml, commented out by default

--- k3dsdk/serialization_xml.cpp
+++ k3dsdk/serialization_xml.cpp
@@ -43,6 +43,7 @@
 #include "mesh_selection.h"
 #include "named_array_types.h"
 #include "nurbs_curve.h"
+#include "nurbs_patch.h"
 #include "plugins.h"
 #include "properties.h"
 #include "result.h"
@@ -2245,6 +2246,8 @@ void load(mesh& Mesh, element& Container, const ipersistent::load_context& Conte
 
 	if(element* const container = find_element(Container, "nurbs_curve_groups"))
 	{
+		assert_not_implemented();
+/*
 		boost::scoped_ptr<nurbs_curve::primitive> nurbs_curves(nurbs_curve::create(Mesh));
 
 		detail::load_array(*container, "materials", nurbs_curves->material, Context);
@@ -2258,7 +2261,7 @@ void load(mesh& Mesh, element& Container, const ipersistent::load_context& Conte
 		detail::load_array(*container, "curve_points", nurbs_curves->curve_points, Context);
 		detail::load_array(*container, "curve_point_weights", nurbs_curves->curve_point_weights, Context);
 		detail::load_array(*container, "curve_knots", nurbs_curves->curve_knots, Context);
-
+*/
 	}
 
 	if(element* const container = find_element(Container, "bilinear_patches"))
@@ -2294,8 +2297,9 @@ void load(mesh& Mesh, element& Container, const ipersistent::load_context& Conte
 	if(element* const container = find_element(Container, "nurbs_patches"))
 	{
 		assert_not_implemented();
-/*
-		mesh::nurbs_patches_t* const nurbs_patches = &Mesh.nurbs_patches.create();
+/*		
+		boost::scoped_ptr<nurbs_patch::primitive> nurbs_patches(nurbs_patch::create(Mesh));
+		
 		detail::load_array(*container, "patch_first_points", nurbs_patches->patch_first_points, Context);
 		detail::load_array(*container, "patch_u_point_counts", nurbs_patches->patch_u_point_counts, Context);
 		detail::load_array(*container, "patch_v_point_counts", nurbs_patches->patch_v_point_counts, Context);
@@ -2303,7 +2307,7 @@ void load(mesh& Mesh, element& Container, const ipersistent::load_context& Conte
 		detail::load_array(*container, "patch_v_orders", nurbs_patches->patch_v_orders, Context);
 		detail::load_array(*container, "patch_u_first_knots", nurbs_patches->patch_u_first_knots, Context);
 		detail::load_array(*container, "patch_v_first_knots", nurbs_patches->patch_v_first_knots, Context);
-		detail::load_array(*container, "patch_selection", nurbs_patches->patch_selection, Context);
+		detail::load_array(*container, "patch_selection", nurbs_patches->patch_selections, Context);
 		detail::load_array(*container, "patch_materials", nurbs_patches->patch_materials, Context);
 		detail::load_arrays(*container, "constant_data", nurbs_patches->constant_data, Context);
 		detail::load_arrays(*container, "uniform_data", nurbs_patches->uniform_data, Context);
@@ -2312,21 +2316,21 @@ void load(mesh& Mesh, element& Container, const ipersistent::load_context& Conte
 		detail::load_array(*container, "patch_u_knots", nurbs_patches->patch_u_knots, Context);
 		detail::load_array(*container, "patch_v_knots", nurbs_patches->patch_v_knots, Context);
 		detail::load_arrays(*container, "varying_data", nurbs_patches->varying_data, Context);
-		detail::load_array(*container, "patch_trim_curve_loop_counts", nurbs_patches->patch_trim_curve_loop_counts, Context);
-		detail::load_array(*container, "patch_first_trim_curve_loops", nurbs_patches->patch_first_trim_curve_loops, Context);
-		detail::load_array(*container, "trim_points", nurbs_patches->trim_points, Context);
-		detail::load_array(*container, "trim_point_selection", nurbs_patches->trim_point_selection, Context);
-		detail::load_array(*container, "first_trim_curves", nurbs_patches->first_trim_curves, Context);
-		detail::load_array(*container, "trim_curve_counts", nurbs_patches->trim_curve_counts, Context);
-		detail::load_array(*container, "trim_curve_loop_selection", nurbs_patches->trim_curve_loop_selection, Context);
-		detail::load_array(*container, "trim_curve_first_points", nurbs_patches->trim_curve_first_points, Context);
-		detail::load_array(*container, "trim_curve_point_counts", nurbs_patches->trim_curve_point_counts, Context);
-		detail::load_array(*container, "trim_curve_orders", nurbs_patches->trim_curve_orders, Context);
-		detail::load_array(*container, "trim_curve_first_knots", nurbs_patches->trim_curve_first_knots, Context);
-		detail::load_array(*container, "trim_curve_selection", nurbs_patches->trim_curve_selection, Context);
-		detail::load_array(*container, "trim_curve_points", nurbs_patches->trim_curve_points, Context);
-		detail::load_array(*container, "trim_curve_point_weights", nurbs_patches->trim_curve_point_weights, Context);
-		detail::load_array(*container, "trim_curve_knots", nurbs_patches->trim_curve_knots, Context);
+		detail::load_array(*container, "patch_trim_curve_loop_counts", nurbs_patches->patch_trim_loop_counts, Context);
+		detail::load_array(*container, "patch_first_trim_curve_loops", nurbs_patches->patch_first_trim_loops, Context);
+		detail::load_array(*container, "trim_points", nurbs_patches->points, Context);
+		detail::load_array(*container, "trim_point_selection", nurbs_patches->point_selections, Context);
+		detail::load_array(*container, "first_trim_curves", nurbs_patches->trim_loop_first_curves, Context);
+		detail::load_array(*container, "trim_curve_counts", nurbs_patches->trim_loop_curve_counts, Context);
+		detail::load_array(*container, "trim_curve_loop_selection", nurbs_patches->trim_loop_selections, Context);
+		detail::load_array(*container, "trim_curve_first_points", nurbs_patches->curve_first_points, Context);
+		detail::load_array(*container, "trim_curve_point_counts", nurbs_patches->curve_point_counts, Context);
+		detail::load_array(*container, "trim_curve_orders", nurbs_patches->curve_orders, Context);
+		detail::load_array(*container, "trim_curve_first_knots", nurbs_patches->curve_first_knots, Context);
+		detail::load_array(*container, "trim_curve_selection", nurbs_patches->curve_selections, Context);
+		detail::load_array(*container, "trim_curve_points", nurbs_patches->curve_points, Context);
+		detail::load_array(*container, "trim_curve_point_weights", nurbs_patches->curve_point_weights, Context);
+		detail::load_array(*container, "trim_curve_knots", nurbs_patches->curve_knots, Context);
 */
 	}
 
