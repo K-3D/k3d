bee0ef31b0d90bc193cc461def298f20db23983b
Timothy M. Shead <tshead@k-3d.com>
2009-05-13 02:40:09 +0000

    COMP: Remove obsolete NGUI regression tests.

--- tests/ngui.assign_hotkeys.py
+++
@@ -1,6 +0,0 @@
-#python
-
-import k3d
-
-k3d.get_command_node("/document/window/menus/assign_hotkeys").execute_command("activate", "")
-k3d.get_command_node("/assign_hotkeys/close").execute_command("activate", "")
