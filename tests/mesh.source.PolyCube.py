515aed34782cda367a9b27e3e63fe60f4ec73bc5
Timothy M. Shead <tshead@k-3d.com>
2009-05-17 22:15:15 +0000

    COMP: Fix broken PolyCube test.

--- tests/mesh.source.PolyCube.py
+++ tests/mesh.source.PolyCube.py
@@ -4,6 +4,5 @@ import testing
 
 setup = testing.setup_mesh_source_test("PolyCube")
 
-testing.assert_contains_solid_polyhedron(setup.output_mesh)
 testing.mesh_comparison_to_reference(setup.document, setup.source.get_property("output_mesh"), "mesh.source.PolyCube", 1)
 
