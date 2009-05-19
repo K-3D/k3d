69fddde063a1ef5f78ffcbdf2fe7347e870b313d
Timothy M. Shead <tshead@k-3d.com>
2009-05-19 03:59:00 +0000

    COMP: Fix broken LSystemParser test, and improve its performance.

--- tests/mesh.source.LSystemParser.py
+++ tests/mesh.source.LSystemParser.py
@@ -3,5 +3,6 @@
 import testing
 
 setup = testing.setup_mesh_source_test("LSystemParser")
+setup.source.growth = 2
 testing.mesh_comparison_to_reference(setup.document, setup.source.get_property("output_mesh"), "mesh.source.LSystemParser", 16)
 
