a772ab0c22f693db4564ff8d3bc53e944cbe68e4
Timothy M. Shead <tshead@k-3d.com>
2009-05-20 00:27:17 +0000

    COMP: Fix broken TriangulateFaces.self_intersection test.

--- tests/mesh.modifier.TriangulateFaces.self_intersection.py
+++ tests/mesh.modifier.TriangulateFaces.self_intersection.py
@@ -17,42 +17,20 @@ for position in positions:
 	points.append(k3d.point3(position[0], position[1], position[2]))
 	point_selection.append(0.0)
 
-polyhedra = mesh.create_polyhedra()
+polyhedron = k3d.polyhedron.create(mesh)
+polyhedron.shell_first_faces().append(0)
+polyhedron.shell_face_counts().append(1)
+polyhedron.shell_types().append(k3d.polyhedron.shell_type.POLYGONS)
+polyhedron.face_first_loops().append(0)
+polyhedron.face_loop_counts().append(1)
+polyhedron.face_materials().append(None)
+polyhedron.face_selections().append(0.0)
+polyhedron.loop_first_edges().append(0)
+polyhedron.edge_points().assign([0, 1, 2, 3])
+polyhedron.clockwise_edges().assign([1, 2, 3, 0])
+polyhedron.edge_selections().assign([0.0, 0.0, 0.0, 0.0])
 
-first_faces = polyhedra.create_first_faces()
-first_faces.assign([0])
-
-face_counts = polyhedra.create_face_counts()
-face_counts.assign([1])
-
-types = polyhedra.create_types()
-types.assign([k3d.polyhedron_type.polygons])
-
-face_first_loops = polyhedra.create_face_first_loops()
-face_first_loops.assign([0])
-
-face_loop_counts = polyhedra.create_face_loop_counts()
-face_loop_counts.assign([1])
-
-face_materials = polyhedra.create_face_materials()
-face_materials.assign([None])
-
-face_selection = polyhedra.create_face_selection()
-face_selection.assign([0.0])
-
-loop_first_edges = polyhedra.create_loop_first_edges()
-loop_first_edges.assign([0])
-
-edge_points = polyhedra.create_edge_points()
-edge_points.assign([0, 1, 2, 3])
-
-clockwise_edges = polyhedra.create_clockwise_edges()
-clockwise_edges.assign([1, 2, 3, 0])
-
-edge_selection = polyhedra.create_edge_selection()
-edge_selection.assign([0.0, 0.0, 0.0, 0.0])
-
-Cs = polyhedra.writable_face_varying_data().create_array("Cs", "k3d::color")
+Cs = polyhedron.face_varying_data().create_array("Cs", "k3d::color")
 Cs.assign([k3d.color(1, 0, 0), k3d.color(0, 1, 0), k3d.color(0, 0, 1), k3d.color(1, 1, 1)])
 
 modifier = document.new_node("TriangulateFaces")
