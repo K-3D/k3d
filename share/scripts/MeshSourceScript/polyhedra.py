ddabf7688271209666bfc34daeabaca146b3072e
Timothy M. Shead <tshead@k-3d.com>
2009-05-20 00:00:31 +0000

    COMP: Fix broken MeshSourceScript.polyhedra test.

--- share/scripts/MeshSourceScript/polyhedra.py
+++ share/scripts/MeshSourceScript/polyhedra.py
@@ -7,57 +7,45 @@ k3d.check_node_environment(locals(), "MeshSourceScript")
 points = Output.create_points()
 point_selection = Output.create_point_selection()
 
-# Perform required one-time setup to store polyhedra in the mesh ...
-polyhedra = Output.create_polyhedra()
-first_faces = polyhedra.create_first_faces()
-face_counts = polyhedra.create_face_counts()
-types = polyhedra.create_types()
-face_first_loops = polyhedra.create_face_first_loops()
-face_loop_counts = polyhedra.create_face_loop_counts()
-face_materials = polyhedra.create_face_materials()
-face_selection = polyhedra.create_face_selection()
-loop_first_edges = polyhedra.create_loop_first_edges()
-edge_points = polyhedra.create_edge_points()
-clockwise_edges = polyhedra.create_clockwise_edges()
-edge_selection = polyhedra.create_edge_selection()
-
-# Create an (optional) array to store uniform (per-face) colors ...
-Cs = polyhedra.writable_uniform_data().create("Cs", "k3d::color")
-
 # Create two polyhedra ...
 for i in range(2):
-	first_faces.append(len(face_first_loops))
-	face_counts.append(2)
-	types.append(k3d.polyhedron_type.polygons)
+	polyhedron = k3d.polyhedron.create(Output)
+
+	# Create an (optional) array to store uniform (per-face) colors ...
+	Cs = polyhedron.uniform_data().create("Cs", "k3d::color")
+
+	polyhedron.shell_first_faces().append(len(polyhedron.face_first_loops()))
+	polyhedron.shell_face_counts().append(3)
+	polyhedron.shell_types().append(k3d.polyhedron.shell_type.POLYGONS)
 
 	# Create three faces in each polyhedron ...
 	for j in range(3):
 		# Each face has a single loop (its exterior boundary) ...
-		face_first_loops.append(len(loop_first_edges))
-		face_loop_counts.append(1)
-		face_materials.append(None)
-		face_selection.append(0.0)
+		polyhedron.face_first_loops().append(len(polyhedron.loop_first_edges()))
+		polyhedron.face_loop_counts().append(1)
+		polyhedron.face_materials().append(None)
+		polyhedron.face_selections().append(0.0)
 
 		Cs.append(k3d.color(1, j / 2.0, i / 1.0))
 
-		loop_first_edges.append(len(edge_points))
+		polyhedron.loop_first_edges().append(len(polyhedron.edge_points()))
 
 		# Each loop has four edges, each of which points to the next edge in clockwise-order ...
-		clockwise_edges.append(len(edge_points) + 1)
-		clockwise_edges.append(len(edge_points) + 2)
-		clockwise_edges.append(len(edge_points) + 3)
-		clockwise_edges.append(len(edge_points) + 0)
+		polyhedron.clockwise_edges().append(len(polyhedron.edge_points()) + 1)
+		polyhedron.clockwise_edges().append(len(polyhedron.edge_points()) + 2)
+		polyhedron.clockwise_edges().append(len(polyhedron.edge_points()) + 3)
+		polyhedron.clockwise_edges().append(len(polyhedron.edge_points()) + 0)
 
 		# Each edge refers to its starting-point ...
-		edge_points.append(len(points) + 0)
-		edge_points.append(len(points) + 1)
-		edge_points.append(len(points) + 2)
-		edge_points.append(len(points) + 3)
+		polyhedron.edge_points().append(len(points) + 0)
+		polyhedron.edge_points().append(len(points) + 1)
+		polyhedron.edge_points().append(len(points) + 2)
+		polyhedron.edge_points().append(len(points) + 3)
 
-		edge_selection.append(0.0)
-		edge_selection.append(0.0)
-		edge_selection.append(0.0)
-		edge_selection.append(0.0)
+		polyhedron.edge_selections().append(0.0)
+		polyhedron.edge_selections().append(0.0)
+		polyhedron.edge_selections().append(0.0)
+		polyhedron.edge_selections().append(0.0)
 
 		positions = [(-5, 0, 5), (5, 0, 5), (5, 0, -5), (-5, 0, -5)]
 
