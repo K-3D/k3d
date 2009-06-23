b8d590b1efa880bd4992d5b614ebfdc340fa36eb
Bart Janssens <bart.janssens@lid.kviv.be>
2009-06-23 23:06:43 +0000

    COMP: Make the basic Delete test work again. TODO: get the complex test working as well

--- tests/mesh.modifier.Delete.complex.py
+++ tests/mesh.modifier.Delete.complex.py
@@ -19,9 +19,29 @@ modifier = document.new_node("Delete")
 
 # select some faces, distributed along polyhedra
 selection = k3d.mesh_selection.deselect_all()
-selection.faces = [(0, 6, 0), (6, 7, 1), (7, 8, 0), (8, 9, 1), (9, 13, 0), (13, 15, 1), (15, 16, 0), (16, 17, 1), (17, 19, 0), (19, 20, 1), (20, 23, 0)]
-selection.points = [(0, 7, 0), (7, 8, 1), (8, 10000, 0)]
-selection.edges = [(0, 25, 0), (25, 26, 1), (26, 10000, 0)]
+face_selection = k3d.mesh_selection.component(0, 1000, k3d.selection.type.UNIFORM)
+face_selection.add_range(0, 6, 0)
+face_selection.add_range(6, 7, 1)
+face_selection.add_range(7, 8, 0)
+face_selection.add_range(8, 9, 1)
+face_selection.add_range(9, 13, 0)
+face_selection.add_range(13, 15, 1)
+face_selection.add_range(15, 16, 0)
+face_selection.add_range(16, 17, 1)
+face_selection.add_range(17, 19, 0)
+face_selection.add_range(19, 20, 1)
+face_selection.add_range(20, 23, 0)
+point_selection = k3d.mesh_selection.component(0, 1000, k3d.selection.type.POINT)
+point_selection.add_range(0, 7, 0)
+point_selection.add_range(7, 8, 1)
+point_selection.add_range(8, 10000, 0)
+edge_selection = k3d.mesh_selection.component(0, 1000, k3d.selection.type.SPLIT_EDGE)
+edge_selection.add_range(0, 25, 0)
+edge_selection.add_range(25, 26, 1)
+edge_selection.add_range(26, 10000, 0)
+selection.add_component(face_selection)
+selection.add_component(point_selection)
+selection.add_component(edge_selection)
 modifier.mesh_selection = selection
 
 document.set_dependency(modifier.get_property("input_mesh"), varying_colors.get_property("output_mesh"))
--- tests/mesh.modifier.Delete.py
+++ tests/mesh.modifier.Delete.py
@@ -11,7 +11,8 @@ modifier = document.new_node("Delete")
 
 # select some faces, distributed along polyhedra
 selection = k3d.mesh_selection.deselect_all()
-selection.faces = [(12, 13, 1)]
+face_selection = k3d.mesh_selection.component(0, 1000, k3d.selection.type.UNIFORM, 12, 13, 1)
+selection.add_component(face_selection)
 modifier.mesh_selection = selection
 
 document.set_dependency(modifier.get_property("input_mesh"), source.get_property("output_mesh"))
