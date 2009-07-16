7945bc7b9277c7f91c50e68f817e97b996aff777
Timothy M. Shead <tshead@k-3d.com>
2009-07-16 00:03:06 -0600

    COMP: Fix SmoothPoints test.

--- tests/mesh.modifier.SmoothPoints.py
+++ tests/mesh.modifier.SmoothPoints.py
@@ -9,8 +9,8 @@ setup.source.rows = 2
 setup.source.columns = 2
 setup.source.slices = 2
 
-selection = k3d.mesh_selection.deselect_all()
-selection.points = k3d.mesh_selection.component_select_all()
+selection = k3d.geometry.uniform_selection(0)
+selection.points = k3d.geometry.point_selection.uniform(selection, 1)
 
 setup.modifier.mesh_selection = selection
 
