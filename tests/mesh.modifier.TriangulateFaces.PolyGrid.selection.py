43984c96c9bd2e6f29735544e90d894d1255de97
Timothy M. Shead <tshead@k-3d.com>
2009-07-17 00:07:57 -0600

    COMP: Fix TriangulateFaces tests.

--- tests/mesh.modifier.TriangulateFaces.PolyGrid.selection.py
+++ tests/mesh.modifier.TriangulateFaces.PolyGrid.selection.py
@@ -7,9 +7,19 @@ setup = testing.setup_mesh_modifier_test("PolyGrid", "TriangulateFaces")
 setup.source.rows = 3
 setup.source.columns = 3
 
-mesh_selection = k3d.mesh_selection.deselect_all()
-mesh_selection.faces = [ (0, 3, 1) ]
-setup.modifier.mesh_selection = mesh_selection
+selection = k3d.geometry.uniform_selection(0)
+face_selection = k3d.geometry.primitive_selection.create(selection)
+face_selection.primitive_begin().append(0)
+face_selection.primitive_end().append(1)
+face_selection.primitive_selection_type().append(k3d.selection.type.UNIFORM)
+face_selection.primitive_first_range().append(0)
+face_selection.primitive_range_count().append(1)
+face_selection.index_begin().append(0)
+face_selection.index_end().append(3)
+face_selection.weight().append(1)
+print str(selection)
+
+setup.modifier.mesh_selection = selection
 
 testing.mesh_comparison_to_reference(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.TriangulateFaces.PolyGrid.selection", 2)
 
