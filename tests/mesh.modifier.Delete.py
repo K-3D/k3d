f603c157789667216b2f2f98c1668d97c3ae6f88
Timothy M. Shead <tshead@k-3d.com>
2009-07-18 09:32:29 -0600

    COMP: Fix mesh.modifier.Delete test.

--- tests/mesh.modifier.Delete.py
+++ tests/mesh.modifier.Delete.py
@@ -11,8 +11,10 @@ modifier = document.new_node("Delete")
 
 # select some faces, distributed along polyhedra
 selection = k3d.geometry.selection.create(0)
+
 face_selection = k3d.geometry.primitive_selection.create(selection, k3d.selection.type.UNIFORM)
 k3d.geometry.primitive_selection.append(face_selection, 12, 13, 1)
+
 modifier.mesh_selection = selection
 
 document.set_dependency(modifier.get_property("input_mesh"), source.get_property("output_mesh"))
