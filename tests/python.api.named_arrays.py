c7d7a95c8de306fd60e994bd4f305667e0e63828
Timothy M. Shead <tshead@k-3d.com>
2009-07-22 23:39:17 -0600

    COMP: Fix python.api.named_arrays test.

--- tests/python.api.named_arrays.py
+++ tests/python.api.named_arrays.py
@@ -11,11 +11,11 @@ polyhedron = k3d.polyhedron.create(mesh)
 named_array_types = [ "k3d::bool_t", "k3d::color", "k3d::double_t", "k3d::inode*", "k3d::int16_t", "k3d::int32_t", "k3d::int64_t", "k3d::int8_t", "k3d::matrix4", "k3d::normal3", "k3d::point2", "k3d::point3", "k3d::point4", "k3d::string_t", "k3d::texture3", "k3d::uint16_t", "k3d::uint32_t", "k3d::uint64_t", "k3d::uint8_t", "k3d::vector2", "k3d::vector3", "k3d::uint_t" ]
 
 for type in named_array_types:
-	array = polyhedron.face_varying_data().create(type, type)
+	array = polyhedron.face_varying_attributes().create(type, type)
 
 for type in named_array_types:
-	array = polyhedron.face_varying_data()[type]
+	array = polyhedron.face_varying_attributes()[type]
 
 for type in named_array_types:
-	const_array = polyhedron.face_varying_data()[type]
+	const_array = polyhedron.face_varying_attributes()[type]
 
