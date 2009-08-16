36bcb67675c9f81c80c0ee98915c22824656e541
Bart Janssens <bart.janssens@lid.kviv.be>
2009-08-16 13:01:48 +0200

    COMP: Update SubdivideEdges test

--- tests/mesh.modifier.SubdivideEdges.py
+++ tests/mesh.modifier.SubdivideEdges.py
@@ -25,7 +25,11 @@ document.set_dependency(varying_colors.get_property("input_mesh"), reader.get_pr
 modifier = document.new_node("SubdivideEdges")
 # select some edges, distributed along polyhedra
 selection = k3d.geometry.selection.create(0)
-selection.edges =[(0, 4294967295, 0), (0, 1, 1), (1, 2, 1), (2, 3, 1), (3, 4, 1), (52, 53, 1), (53, 54, 1), (54, 55, 1), (55, 56, 1), (56, 57, 1), (57, 58, 1), (58, 59, 1), (59, 60, 1), (60, 61, 1), (61, 62, 1), (62, 63, 1), (63, 64, 1), (64, 65, 1), (74, 75, 1), (75, 76, 1), (76, 77, 1), (77, 78, 1), (78, 79, 1), (79, 80, 1), (80, 81, 1), (81, 82, 1)]
+edge_selection = k3d.geometry.primitive_selection.create(selection, k3d.selection.type.FACE_VARYING)
+k3d.geometry.primitive_selection.append(edge_selection, 0, 4294967295, 0)
+k3d.geometry.primitive_selection.append(edge_selection, 0, 4, 1)
+k3d.geometry.primitive_selection.append(edge_selection, 52, 65, 1)
+k3d.geometry.primitive_selection.append(edge_selection, 74, 82, 1)
 modifier.mesh_selection = selection
 modifier.vertices = 2
 
