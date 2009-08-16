4415559e8c1d87161d6865cd9eac3c5cb9542a1c
Bart Janssens <bart.janssens@lid.kviv.be>
2009-08-16 15:15:34 +0200

    COMP: Update SubdivideEdges test again

--- share/scripts/MeshModifierScript/random_face_varying_colors.py
+++ share/scripts/MeshModifierScript/random_face_varying_colors.py
@@ -15,7 +15,7 @@ for prim_idx, const_primitive in enumerate(Input.primitives()):
 	if const_primitive.type() == "polyhedron":
 		polyhedron = k3d.polyhedron.validate(Output, Output.primitives()[prim_idx])
 		if polyhedron:
-			Cs = polyhedron.face_varying_attributes().create("Cs", "k3d::color")
+			Cs = polyhedron.varying_attributes().create("Cs", "k3d::color")
 			for i in range(len(polyhedron.edge_points())):
 				Cs.append(colors[i % len(colors)])
 
