4696834b2932979b0551a7147b18bcc9783fad1e
Bart Janssens <bart.janssens@lid.kviv.be>
2009-08-17 23:01:29 +0200

    ENH: Simplify the first lipOrientation implementation, making it more robust

--- modules/polyhedron/fix_orientation.cpp
+++ modules/polyhedron/fix_orientation.cpp
@@ -21,13 +21,10 @@
 		\author Bart Janssens (bart.janssens@lid.kviv.be)
 */
 
-#include <k3dsdk/table_copier.h>
 #include <k3dsdk/basic_math.h>
 #include <k3dsdk/document_plugin_factory.h>
 #include <k3dsdk/geometry.h>
 #include <k3dsdk/imaterial.h>
-#include <k3dsdk/ipipeline_profiler.h>
-#include <k3dsdk/measurement.h>
 #include <k3dsdk/mesh_modifier.h>
 #include <k3dsdk/mesh_selection_sink.h>
 #include <k3dsdk/node.h>
@@ -77,24 +74,19 @@ void fix_orientation(k3d::polyhedron::primitive& Output, const k3d::polyhedron::
 
 	const k3d::uint_t edge_count = boundary_edges.size();
 
-	// Store all boundaries in a separate list, and collect counter-clockwise edges
-	k3d::mesh::indices_t boundary_edge_list;
+	// Collect counter-clockwise edges
 	k3d::mesh::indices_t counter_clockwise_edges(edge_count);
 	for(k3d::uint_t edge = 0; edge != edge_count; ++edge)
 	{
-		if(boundary_edges[edge])
-			boundary_edge_list.push_back(edge);
 		counter_clockwise_edges[Input.clockwise_edges[edge]] = edge;
 	}
-	const k3d::uint_t boundary_edge_count = boundary_edge_list.size();
 
-	// For each boundary edge, tag all edges in its shell
+	// For each edge, tag all edges in its shell
 	k3d::mesh::indices_t edge_shells(edge_count);
 	k3d::mesh::bools_t tagged_edges(edge_count, false);
 	std::vector<k3d::mesh::indices_t> shells;
-	for(k3d::uint_t i = 0; i != boundary_edge_count; ++i)
+	for(k3d::uint_t edge = 0; edge != edge_count; ++edge)
 	{
-		const k3d::uint_t edge = boundary_edge_list[i];
 		if(!tagged_edges[edge])
 		{
 			k3d::mesh::indices_t shell_edges;
@@ -105,28 +97,13 @@ void fix_orientation(k3d::polyhedron::primitive& Output, const k3d::polyhedron::
 	}
 
 	const k3d::uint_t shell_count = shells.size();
-	k3d::log() << debug << "shell counts:" << std::endl;
 	k3d::uint_t total = 0;
 	for(k3d::uint_t shell = 0; shell != shell_count; ++shell)
 	{
-		k3d::log() << debug << "  shell " << shell << ": " << shells[shell].size() << std::endl;
 		total += shells[shell].size();
 	}
-	k3d::log() << debug << "total is " << total << ", mesh count is " << edge_count << std::endl;
-	k3d::uint_t untagged_count = 0;
-	if(total != edge_count)
-	{
-		//k3d::log() << debug << "untagged edges: " << std::endl;
-		for(k3d::uint_t edge = 0; edge != edge_count; ++edge)
-		{
-			if(!tagged_edges[edge])
-			{
-				//k3d::log() << "  " << edge << std::endl;
-				++untagged_count;
-			}
-		}
-	}
-	k3d::log() << debug << "untagged count: " << untagged_count << std::endl;
+	return_if_fail(total == edge_count);
+	k3d::log() << debug << "FixOrientation: found " << shell_count << " shells" << std::endl;
 
 	// Mark the shells with correct orientation, i.e. shells with selected faces
 	k3d::mesh::bools_t valid_shells(shell_count, false);
@@ -137,37 +114,13 @@ void fix_orientation(k3d::polyhedron::primitive& Output, const k3d::polyhedron::
 			valid_shells[edge_shells[Input.loop_first_edges[Input.face_first_loops[face]]]] = true;
 	}
 
-	// Keeps track of the shells that have been fixed
-	k3d::mesh::bools_t fixed_shells = valid_shells;
-
-	for(k3d::uint_t i = 0; i != boundary_edge_count; ++i)
+	for(k3d::uint_t shell = 0; shell != shell_count; ++shell)
 	{
-		const k3d::uint_t edge = boundary_edge_list[i];
-		const k3d::uint_t start = Input.edge_points[edge];
-		const k3d::uint_t end = Input.edge_points[Input.clockwise_edges[edge]];
-		const k3d::uint_t shell = edge_shells[edge];
-		for(k3d::uint_t j = 0; j != boundary_edge_count; ++j)
+		if(!valid_shells[shell])
 		{
-			const k3d::uint_t other_edge = boundary_edge_list[j];
-			const k3d::uint_t other_start = Input.edge_points[other_edge];
-			const k3d::uint_t other_end = Input.edge_points[Input.clockwise_edges[other_edge]];
-			const k3d::uint_t other_shell = edge_shells[other_edge];
-			if(other_edge != edge && other_start == start && other_end == end)
-			{
-				return_if_fail(other_shell != shell);
-				if(valid_shells[shell] && valid_shells[other_shell])
-				{
-					k3d::log() << error << "FixOrientation: Faces with opposite orientation are both marked as correct";
-				}
-				if(valid_shells[shell] && !valid_shells[other_shell] && !fixed_shells[other_shell])
-				{
-					fix_shell(Output, Input, shells[other_shell], counter_clockwise_edges);
-					fixed_shells[other_shell] = true;
-				}
-			}
+			fix_shell(Output, Input, shells[shell], counter_clockwise_edges);
 		}
 	}
-
 }
 
 } // namespace detail
@@ -216,7 +169,7 @@ public:
 			k3d::interface_list<k3d::imesh_sink > > > factory(
 				k3d::uuid(0x919c2a62, 0x97488804, 0x1ff6b580, 0x4c890bb2),
 				"FixOrientation",
-				_("Fix orientation, based on one or more correctly-oriented selected faces"),
+				_("Fix orientation, based on one or more correctly-oriented selected faces. Any shell without any selected face will have the orientation of all of its faces flipped"),
 				"Mesh",
 				k3d::iplugin_factory::STABLE);
 
