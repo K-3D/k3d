04dfbfc26d0afb1687b5da87828c74f86fe675a8
Bart Janssens <bart.janssens@lid.kviv.be>
2009-08-17 23:02:13 +0200

    ENH: Convert FlipOrientation to the new mesh structure

--- modules/mesh/flip_orientation.cpp
+++ modules/mesh/flip_orientation.cpp
@@ -20,16 +20,24 @@
 /** \file
 		\author Timothy M. Shead (tshead@k-3d.com)
 		\author Romain Behar (romainbehar@yahoo.com)
+		\author Bart Janssens (bart.janssens@lid.kviv.be)
 */
 
+#include <k3dsdk/basic_math.h>
+#include <k3dsdk/bicubic_patch.h>
+#include <k3dsdk/bilinear_patch.h>
 #include <k3dsdk/document_plugin_factory.h>
-#include <k3d-i18n-config.h>
 #include <k3dsdk/geometry.h>
-#include <k3dsdk/measurement.h>
-#include <k3dsdk/legacy_mesh.h>
-#include <k3dsdk/legacy_mesh_modifier.h>
+#include <k3dsdk/imaterial.h>
+#include <k3dsdk/mesh_modifier.h>
 #include <k3dsdk/mesh_selection_sink.h>
 #include <k3dsdk/node.h>
+#include <k3dsdk/polyhedron.h>
+#include <k3dsdk/selection.h>
+#include <k3dsdk/utility.h>
+#include <k3dsdk/vectors.h>
+
+#include <boost/scoped_ptr.hpp>
 
 namespace module
 {
@@ -37,13 +45,78 @@ namespace module
 namespace mesh
 {
 
+namespace detail
+{
+
+void flip_polyhedron(k3d::polyhedron::primitive& Output, const k3d::polyhedron::const_primitive& Input)
+{
+	const k3d::uint_t edge_count = Input.edge_points.size();
+	k3d::mesh::indices_t counter_clockwise_edges(edge_count);
+	for(k3d::uint_t edge = 0; edge != edge_count; ++edge)
+	{
+		counter_clockwise_edges[Input.clockwise_edges[edge]] = edge;
+	}
+	for(k3d::uint_t face = 0; face != Input.face_first_loops.size(); ++face)
+	{
+		if(!Output.face_selections[face])
+			continue;
+		const k3d::uint_t loop_begin = Input.face_first_loops[face];
+		const k3d::uint_t loop_end = loop_begin + Input.face_loop_counts[face];
+		for(k3d::uint_t loop = loop_begin; loop != loop_end; ++loop)
+		{
+			const k3d::uint_t first_edge = Input.loop_first_edges[loop];
+			for(k3d::uint_t edge = first_edge; ;)
+			{
+				Output.edge_points[edge] = Input.edge_points[Input.clockwise_edges[edge]];
+				Output.clockwise_edges[edge] = counter_clockwise_edges[edge];
+
+				edge = Input.clockwise_edges[edge];
+				if(edge == first_edge)
+					break;
+			}
+		}
+	}
+}
+
+void flip_bilinear_patch(k3d::bilinear_patch::primitive& Output)
+{
+	for(k3d::uint_t patch = 0; patch != Output.patch_selections.size(); ++patch)
+	{
+		if(!Output.patch_selections[patch])
+			continue;
+		const k3d::uint_t patch_start = 4*patch;
+		std::swap(Output.patch_points[patch_start], Output.patch_points[patch_start + 1]);
+		std::swap(Output.patch_points[patch_start + 2], Output.patch_points[patch_start + 3]);
+	}
+}
+
+void flip_bicubic_patch(k3d::bicubic_patch::primitive& Output)
+{
+	for(k3d::uint_t patch = 0; patch != Output.patch_selections.size(); ++patch)
+	{
+		if(!Output.patch_selections[patch])
+			continue;
+		const k3d::uint_t patch_start = 16*patch;
+		std::swap(Output.patch_points[patch_start], Output.patch_points[patch_start + 3]);
+		std::swap(Output.patch_points[patch_start + 1], Output.patch_points[patch_start + 2]);
+		std::swap(Output.patch_points[patch_start + 4], Output.patch_points[patch_start + 7]);
+		std::swap(Output.patch_points[patch_start + 5], Output.patch_points[patch_start + 6]);
+		std::swap(Output.patch_points[patch_start + 8], Output.patch_points[patch_start + 11]);
+		std::swap(Output.patch_points[patch_start + 9], Output.patch_points[patch_start + 10]);
+		std::swap(Output.patch_points[patch_start + 12], Output.patch_points[patch_start + 15]);
+		std::swap(Output.patch_points[patch_start + 13], Output.patch_points[patch_start + 14]);
+	}
+}
+
+} // namespace detail
+
 /////////////////////////////////////////////////////////////////////////////
 // flip_orientation_implementation
 
 class flip_orientation_implementation :
-	public k3d::mesh_selection_sink<k3d::legacy::mesh_modifier<k3d::node > >
+	public k3d::mesh_selection_sink<k3d::mesh_modifier<k3d::node > >
 {
-	typedef k3d::mesh_selection_sink<k3d::legacy::mesh_modifier<k3d::node > > base;
+	typedef k3d::mesh_selection_sink<k3d::mesh_modifier<k3d::node > > base;
 
 public:
 	flip_orientation_implementation(k3d::iplugin_factory& Factory, k3d::idocument& Document) :
@@ -52,63 +125,37 @@ public:
 		m_mesh_selection.changed_signal().connect(make_reset_mesh_slot());
 	}
 
-	/** \todo Improve the implementation so we don't have to do this */
-	k3d::iunknown* on_rewrite_hint(iunknown* const Hint)
+	void on_create_mesh(const k3d::mesh& Input, k3d::mesh& Output)
 	{
-		// Force updates to re-allocate our mesh, for simplicity
-		return 0;
-	}
-
-	void on_initialize_mesh(const k3d::legacy::mesh& InputMesh, k3d::legacy::mesh& Mesh)
-	{
-		k3d::legacy::deep_copy(InputMesh, Mesh);
-		k3d::geometry::selection::merge(m_mesh_selection.pipeline_value(), Mesh);
-
-		for(k3d::legacy::mesh::polyhedra_t::const_iterator polyhedron = Mesh.polyhedra.begin(); polyhedron != Mesh.polyhedra.end(); ++polyhedron)
+		Output = Input;
+		k3d::geometry::selection::merge(m_mesh_selection.pipeline_value(), Output); // Merges the current document selection with the mesh
+		for(k3d::uint_t i = 0; i != Input.primitives.size(); ++i)
 		{
-			for(k3d::legacy::polyhedron::faces_t::iterator face = (*polyhedron)->faces.begin(); face != (*polyhedron)->faces.end(); ++face)
+			boost::scoped_ptr<k3d::polyhedron::const_primitive> input_polyhedron(k3d::polyhedron::validate(Input, *Input.primitives[i]));
+			boost::scoped_ptr<k3d::bilinear_patch::const_primitive> input_bilinear_patch(k3d::bilinear_patch::validate(Input, *Input.primitives[i]));
+			boost::scoped_ptr<k3d::bicubic_patch::const_primitive> input_bicubic_patch(k3d::bicubic_patch::validate(Input, *Input.primitives[i]));
+			if(input_polyhedron)
 			{
-				if(!(*face)->selection_weight)
-					continue;
-
-				std::vector<k3d::legacy::split_edge*> edges;
-				std::vector<k3d::legacy::point*> vertices;
-				for(k3d::legacy::split_edge* edge = (*face)->first_edge; edge; edge = edge->face_clockwise)
-				{
-					edges.push_back(edge);
-					vertices.push_back(edge->vertex);
-					if(edge->face_clockwise == (*face)->first_edge)
-						break;
-				}
-
-				for(unsigned long i = 0; i != edges.size(); ++i)
-				{
-					edges[i]->vertex = vertices[(i+1)%vertices.size()];
-					edges[i]->face_clockwise = edges[(i+edges.size()-1)%edges.size()];
-				}
+				boost::scoped_ptr<k3d::polyhedron::primitive> output_polyhedron(k3d::polyhedron::validate(Output, Output.primitives[i]));
+				return_if_fail(output_polyhedron);
+				detail::flip_polyhedron(*output_polyhedron, *input_polyhedron);
+			}
+			else if(input_bilinear_patch)
+			{
+				boost::scoped_ptr<k3d::bilinear_patch::primitive> output_patch(k3d::bilinear_patch::validate(Output, Output.primitives[i]));
+				return_if_fail(output_patch);
+				detail::flip_bilinear_patch(*output_patch);
+			}
+			else if(input_bicubic_patch)
+			{
+				boost::scoped_ptr<k3d::bicubic_patch::primitive> output_patch(k3d::bicubic_patch::validate(Output, Output.primitives[i]));
+				return_if_fail(output_patch);
+				detail::flip_bicubic_patch(*output_patch);
 			}
 		}
-
-		for(k3d::legacy::mesh::bilinear_patches_t::const_iterator patch = Mesh.bilinear_patches.begin(); patch != Mesh.bilinear_patches.end(); ++patch)
-		{
-			std::swap((*patch)->control_points[0], (*patch)->control_points[1]);
-			std::swap((*patch)->control_points[2], (*patch)->control_points[3]);
-		}
-
-		for(k3d::legacy::mesh::bicubic_patches_t::const_iterator patch = Mesh.bicubic_patches.begin(); patch != Mesh.bicubic_patches.end(); ++patch)
-		{
-			std::swap((*patch)->control_points[0], (*patch)->control_points[3]);
-			std::swap((*patch)->control_points[1], (*patch)->control_points[2]);
-			std::swap((*patch)->control_points[4], (*patch)->control_points[7]);
-			std::swap((*patch)->control_points[5], (*patch)->control_points[6]);
-			std::swap((*patch)->control_points[8], (*patch)->control_points[11]);
-			std::swap((*patch)->control_points[9], (*patch)->control_points[10]);
-			std::swap((*patch)->control_points[12], (*patch)->control_points[15]);
-			std::swap((*patch)->control_points[13], (*patch)->control_points[14]);
-		}
 	}
 
-	void on_update_mesh(const k3d::legacy::mesh& InputMesh, k3d::legacy::mesh& Mesh)
+	void on_update_mesh(const k3d::mesh& Input, k3d::mesh& Output)
 	{
 	}
 
@@ -119,7 +166,7 @@ public:
 			k3d::interface_list<k3d::imesh_sink > > > factory(
 				k3d::uuid( 0x7d95cd84, 0xee5a4ad6, 0x86feab94, 0xcbea91ca),
 				"FlipOrientation",
-				_("Reverses the orientation of selected polygons"),
+				_("Reverses the orientation of selected polygons and bicubic or bilinear patches"),
 				"Polygon",
 				k3d::iplugin_factory::STABLE);
 
