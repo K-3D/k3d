2ff918b2afdbfe66909b699c7a16ef214978efa1
Bart Janssens <bart.janssens@lid.kviv.be>
2009-08-30 00:32:10 +0200

    COMP: Commit missing vbo.h/.cpp

---
+++ modules/advanced_opengl_painters/vbo.cpp
@@ -0,0 +1,129 @@
+// K-3D
+// Copyright (c) 1995-2005, Timothy M. Shead
+//
+// Contact: tshead@k-3d.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#include "vbo.h"
+
+#include <k3dsdk/hints.h>
+#include <k3dsdk/polyhedron.h>
+#include <k3dsdk/selection.h>
+#include <k3dsdk/utility_gl.h>
+
+#include <boost/any.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#include <map>
+
+namespace module
+{
+
+namespace opengl
+{
+
+namespace painters
+{
+
+////////
+// class vbo
+///////
+
+vbo::vbo( )
+{
+	glGenBuffers(1, &m_name);
+}
+
+vbo::~ vbo( )
+{
+	glDeleteBuffers(1, &m_name);
+}
+
+vbo::operator GLuint( ) const
+{
+	return m_name;
+}
+
+void validate(const vbo& VBO)
+{
+	if(!glIsBuffer(VBO))
+	{
+		throw vbo_exception("point_vbo is not a buffer!");
+	}
+}
+
+void bind_vertex_buffer(const vbo& VBO)
+{
+	validate(VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexPointer(3, GL_DOUBLE, 0, 0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+}
+
+void clean_vbo_state()
+{
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+	glDisableClientState(GL_SECONDARY_COLOR_ARRAY);
+	glDisableClientState(GL_INDEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	glDisableClientState(GL_FOG_COORDINATE_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_EDGE_FLAG_ARRAY);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+///////////////////////////////
+// point_vbo
+///////////////////////////////
+
+void point_vbo::on_topology_changed(vbo& Output, const k3d::mesh& InputMesh)
+{
+	k3d::log() << debug << "topology changed" << std::endl;
+	const k3d::mesh::points_t& points = *InputMesh.points;
+	glBindBuffer(GL_ARRAY_BUFFER, Output);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points[0]) * points.size(), &points[0], GL_STATIC_DRAW);
+}
+
+void point_vbo::on_selection_changed(vbo& Output, const k3d::mesh& InputMesh) {k3d::log() << debug << "selection changed" << std::endl;}
+
+void point_vbo::on_geometry_changed(vbo& Output, const k3d::mesh& InputMesh, const k3d::mesh::indices_t& ChangedPoints)
+{
+	const k3d::mesh::points_t& points = *InputMesh.points;
+	glBindBuffer(GL_ARRAY_BUFFER, Output);
+	if(ChangedPoints.empty()) // caller didn't tell which points changed, so update everything
+	{
+		k3d::log() << debug << "geometry changed, full rebuild" << std::endl;
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points[0]) * points.size(), &points[0], GL_STATIC_DRAW);
+	}
+	else
+	{
+		k3d::log() << debug << "geometry changed, mapping points" << std::endl;
+		k3d::point3* opengl_points = static_cast<k3d::point3*>(glMapBuffer(GL_ARRAY_BUFFER, GL_READ_WRITE));
+		for (size_t index = 0; index != ChangedPoints.size(); ++index)
+		{
+			opengl_points[ChangedPoints[index]] = points[ChangedPoints[index]];
+		}
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+	}
+}
+
+} // namespace painters
+
+} // namespace opengl
+
+} // namespace module
---
+++ modules/advanced_opengl_painters/vbo.h
@@ -0,0 +1,98 @@
+#ifndef MODULES_ADVANCED_OPENGL_PAINTERS_VBO_H
+#define MODULES_ADVANCED_OPENGL_PAINTERS_VBO_H
+
+// K-3D
+// Copyright (c) 1995-2005, Timothy M. Shead
+//
+// Contact: tshead@k-3d.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+/** \file Helper functions and classes to manage a global data cache for the mesh painters
+ * 	\author Bart Janssens (bart.janssens@lid.kviv.be)
+ */
+
+#include <k3dsdk/gl.h>
+#include <k3dsdk/imesh_painter_gl.h>
+#include <k3dsdk/iproperty.h>
+#include <k3dsdk/mesh.h>
+#include <k3dsdk/properties.h>
+
+#include "painter_cache.h"
+
+#include <stdexcept>
+
+namespace module
+{
+
+namespace opengl
+{
+
+namespace painters
+{
+
+/// Exception for VBO-related errors
+class vbo_exception : public std::runtime_error
+{
+	typedef std::runtime_error base;
+public:
+	vbo_exception(const std::string& Message) : base("VBO error: " + Message) {}
+};
+
+/// Convenience wrapper for OpenGL vertex buffer objects
+class vbo
+{
+public:
+	vbo();
+	~vbo();
+	/// Implicit conversion so we can use a vbo instance as an argument to OpenGL functions
+	operator GLuint() const;
+private:
+	vbo(const vbo&);
+	vbo& operator=(const vbo&);
+
+	GLuint m_name;
+};
+
+/// Bind a vertex buffer VBO, and enable the required OpenGL state
+void bind_vertex_buffer(const vbo& VBO);
+
+/// Disable all VBOs
+void clean_vbo_state();
+
+//////////////////////////////////////////
+// point_vbo
+//////////////////////////////////////////
+
+/// Cached object for point data stored in VBOs
+class point_vbo : public cached_polyhedron_data<vbo>
+{
+	typedef cached_polyhedron_data<vbo> base;
+public:
+	point_vbo(k3d::iproperty::changed_signal_t& ChangedSignal) : base(ChangedSignal) {}
+private:
+private:
+	void on_topology_changed(vbo& Output, const k3d::mesh& InputMesh);
+	void on_selection_changed(vbo& Output, const k3d::mesh& InputMesh);
+	void on_geometry_changed(vbo& Output, const k3d::mesh& InputMesh, const k3d::mesh::indices_t& ChangedPoints);
+};
+
+} // namespace painters
+
+} // namespace opengl
+
+} // namespace module
+
+#endif // !MODULES_ADVANCED_OPENGL_PAINTERS_VBO_H
