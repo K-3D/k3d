d589734475a48b01d05153014657c19300dd40cd
Bart Janssens <bart.janssens@lid.kviv.be>
2010-01-17 18:57:55 +0100

    COMP: Switch to a polygrid to avoid number comparison problems in CatmullClark test

--- tests/mesh.modifier.CatmullClark.py
+++ tests/mesh.modifier.CatmullClark.py
@@ -3,13 +3,13 @@
 import k3d
 import testing
 
-setup = testing.setup_mesh_modifier_test("PolyCylinder", "CatmullClark")
-setup.source.u_segments = 5
+setup = testing.setup_mesh_modifier_test("PolyGrid", "CatmullClark")
+setup.source.u_segments = 4
 setup.source.v_segments = 2
 setup.source.bottom = False
 
 setup.modifier.mesh_selection = k3d.geometry.selection.create(1)
 
 testing.require_valid_mesh(setup.document, setup.modifier.get_property("output_mesh"))
-testing.mesh_reference_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.CatmullClark", 1)
+testing.mesh_reference_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.CatmullClark", 2)
 
