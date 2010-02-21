d2da2bf4abeab4f9fb95a87db519e95fc80b253a
Timothy M. Shead <tshead@k-3d.com>
2010-02-20 22:35:43 -0700

    ENH: STLMeshWriter automatically triangulates its inputs, and mark it stable.

--- modules/stl_io/mesh_writer.cpp
+++ modules/stl_io/mesh_writer.cpp
@@ -30,6 +30,7 @@
 #include <k3dsdk/mesh_writer.h>
 #include <k3dsdk/node.h>
 #include <k3dsdk/polyhedron.h>
+#include <k3dsdk/triangulator.h>
 
 #include <boost/scoped_ptr.hpp>
 
@@ -71,12 +72,54 @@ public:
 			"STLMeshWriter",
 			_("Mesh writer that saves external Stereo Triangulation Lithograph (.stl) files"),
 			"MeshWriter",
-			k3d::iplugin_factory::EXPERIMENTAL);
+			k3d::iplugin_factory::STABLE);
 
 		return factory;
 	}
 
 private:
+	class triangulator :
+		public k3d::triangulator
+	{
+		typedef k3d::triangulator base;
+
+	public:
+		triangulator(const k3d::mesh::strings_t* const FaceGroups, k3d::mesh::points_t& TrianglePoints, k3d::mesh::indices_t& TriangleIndices, k3d::mesh::strings_t& TriangleGroups) :
+			face_groups(FaceGroups),
+			triangle_points(TrianglePoints),
+			triangle_indices(TriangleIndices),
+			triangle_groups(TriangleGroups)
+		{
+		}
+
+	private:
+		void start_face(const k3d::uint_t Face)
+		{
+			current_face = Face;
+		}
+
+		void add_vertex(const k3d::point3& Coordinates, k3d::uint_t Vertices[4], k3d::uint_t Edges[4], double Weights[4], k3d::uint_t& NewVertex)
+		{
+			NewVertex = triangle_points.size();
+			triangle_points.push_back(Coordinates);
+		}
+
+		void add_triangle(k3d::uint_t Vertices[3], k3d::uint_t Edges[3])
+		{
+			triangle_indices.push_back(Vertices[0]);
+			triangle_indices.push_back(Vertices[1]);
+			triangle_indices.push_back(Vertices[2]);
+
+			triangle_groups.push_back(face_groups ? (*face_groups)[current_face] : "default");
+		}
+
+		const k3d::mesh::strings_t* const face_groups;
+		k3d::mesh::points_t& triangle_points;
+		k3d::mesh::indices_t& triangle_indices;
+		k3d::mesh::strings_t& triangle_groups;
+		k3d::uint_t current_face;
+	};
+
 	void on_write_mesh(const k3d::mesh& Input, const k3d::filesystem::path& OutputPath, std::ostream& Output)
 	{
 		for(k3d::mesh::primitives_t::const_iterator primitive = Input.primitives.begin(); primitive != Input.primitives.end(); ++primitive)
@@ -84,56 +127,41 @@ private:
 			boost::scoped_ptr<k3d::polyhedron::const_primitive> polyhedron(k3d::polyhedron::validate(Input, **primitive));
 			if(!polyhedron)
 				continue;
-			
-			if(!k3d::polyhedron::is_triangles(*polyhedron))
-				continue;
 
-			// Group faces together by default, allowing for optional user-specified groups ...
-			k3d::mesh::strings_t face_groups(polyhedron->face_shells.size(), "default");
-			if(m_group_solids.pipeline_value())
+			// Setup our inputs and outputs ...
+			const k3d::mesh::strings_t* const face_groups = m_group_solids.pipeline_value() ? polyhedron->face_attributes.lookup<k3d::mesh::strings_t>(m_group_array.pipeline_value()) : 0;
+
+			k3d::mesh::points_t triangle_points = *Input.points;
+			k3d::mesh::indices_t triangle_indices;
+			k3d::mesh::strings_t triangle_groups;
+
+			// Triangulate the polyhedron ...
+			triangulator(face_groups, triangle_points, triangle_indices, triangle_groups).process(Input, *polyhedron);
+
+			// Get the set of unique face group names ...
+			std::set<k3d::string_t> triangle_group_names(triangle_groups.begin(), triangle_groups.end());
+
+			// For each group of triangles, write-out an STL solid ...
+			const k3d::uint_t triangle_begin = 0;
+			const k3d::uint_t triangle_end = triangle_begin + triangle_groups.size();
+			for(std::set<k3d::string_t>::const_iterator triangle_group_name = triangle_group_names.begin(); triangle_group_name != triangle_group_names.end(); ++triangle_group_name)
 			{
-				const k3d::mesh::strings_t* const group_array = polyhedron->face_attributes.lookup<k3d::mesh::strings_t>(m_group_array.pipeline_value());
-				return_if_fail(group_array);
+				Output << "solid " << *triangle_group_name << "\n";
 
-				face_groups.assign(group_array->begin(), group_array->end());
-			}
-
-			std::set<k3d::string_t> face_group_names(face_groups.begin(), face_groups.end());
-
-			// Compute face normals, so we can write an STL file for maximum portability ...
-			k3d::mesh::normals_t face_normals;
-			k3d::polyhedron::create_face_normal_lookup(Input, *polyhedron, face_normals);
-
-			// Compute counterclockwise edges, since STL faces use right-hand (counterclockwise) winding ...
-			k3d::mesh::indices_t counterclockwise_edges;
-			k3d::polyhedron::create_counterclockwise_edge_lookup(*polyhedron, counterclockwise_edges);
-
-			// For each group of faces, write-out an STL solid ...
-			const k3d::mesh::points_t& points = *Input.points;
-			const k3d::uint_t face_begin = 0;
-			const k3d::uint_t face_end = face_begin + polyhedron->face_shells.size();
-			for(std::set<k3d::string_t>::const_iterator face_group_name = face_group_names.begin(); face_group_name != face_group_names.end(); ++face_group_name)
-			{
-				Output << "solid " << *face_group_name << "\n";
-
-				for(k3d::uint_t face = face_begin; face != face_end; ++face)
+				for(k3d::uint_t triangle = triangle_begin; triangle != triangle_end; ++triangle)
 				{
-					if(face_groups[face] != *face_group_name)
+					if(triangle_groups[triangle] != *triangle_group_name)
 						continue;
 
-					Output << "facet normal " << face_normals[face] << "\n";
+					const k3d::point3 a = triangle_points[triangle_indices[(3 * triangle) + 0]];
+					const k3d::point3 b = triangle_points[triangle_indices[(3 * triangle) + 1]];
+					const k3d::point3 c = triangle_points[triangle_indices[(3 * triangle) + 2]];
+
+					Output << "facet normal " << k3d::polyhedron::normal(a, b, c) << "\n";
 					Output << "  outer loop\n";
-
-					const k3d::uint_t first_edge = polyhedron->loop_first_edges[polyhedron->face_first_loops[face]];
-					for(k3d::uint_t edge = first_edge; ;)
-					{
-						Output << "    vertex " << points[polyhedron->vertex_points[edge]] << "\n";
-
-						edge = counterclockwise_edges[edge];
-						if(edge == first_edge)
-							break;
-					}
-
+					Output << "    vertex " << a << "\n";
+					Output << "    vertex " << c << "\n"; // Note: STL stores faces with right-hand ordering!
+					Output << "    vertex " << b << "\n";
 					Output << "  endloop\n";
 					Output << "endfacet\n";
 				}
