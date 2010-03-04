cf340d475df0992b0b86b1682e2d92fa45bef197
Timothy M. Shead <tshead@k-3d.com>
2010-03-04 00:09:33 -0700

    COMP: First pass at creating a suitable runtime environment for automated testing of demo scripts.

---
+++ tests/python/demo_script_environment.py
@@ -0,0 +1,9 @@
+#python
+
+# This script recreates a "normal" environment that allows us to perform automated
+# regression-testing on our demo scripts.
+
+import k3d
+
+Document = k3d.new_document()
+
