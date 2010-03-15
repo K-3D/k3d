ee9b3fa45d4125fc6e07d4ecf61702707773d6ea
Bart Janssens <bart.janssens@lid.kviv.be>
2010-03-15 06:56:01 +0100

    ENH: Add AddPointAttributes testing plugin

--- modules/test/module.cpp
+++ modules/test/module.cpp
@@ -32,6 +32,7 @@ namespace test
 extern k3d::iplugin_factory& add_color_attribute_factory();
 extern k3d::iplugin_factory& add_color_attributes_factory();
 extern k3d::iplugin_factory& add_index_attributes_factory();
+extern k3d::iplugin_factory& add_point_attributes_factory();
 extern k3d::iplugin_factory& mesh_diff_factory();
 extern k3d::iplugin_factory& mesh_to_stdout_factory();
 extern k3d::iplugin_factory& pipeline_profiler_factory();
@@ -47,6 +48,7 @@ K3D_MODULE_START(Registry)
 	Registry.register_factory(module::test::add_color_attribute_factory());
 	Registry.register_factory(module::test::add_color_attributes_factory());
 	Registry.register_factory(module::test::add_index_attributes_factory());
+	Registry.register_factory(module::test::add_point_attributes_factory());
 	Registry.register_factory(module::test::mesh_diff_factory());
 	Registry.register_factory(module::test::mesh_to_stdout_factory());
 	Registry.register_factory(module::test::pipeline_profiler_factory());
