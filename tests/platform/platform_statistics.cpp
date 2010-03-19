5b1f5887f3adce849c3892864f1e717869aa546c
Timothy M. Shead <tshead@k-3d.com>
2010-03-18 21:50:14 -0600

    ENH: Add a tool for experimenting with statistics algorithms for use with floating-point comparisons.

---
+++ tests/platform/platform_statistics.cpp
@@ -0,0 +1,39 @@
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/median.hpp>
+#include <boost/accumulators/statistics/min.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+using namespace boost::accumulators;
+
+#include <boost/lexical_cast.hpp>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		accumulator_set<double, stats<tag::min, tag::mean, tag::max, tag::median, tag::lazy_variance> > model;
+		for(int i = 1; i < argc; ++i)
+		{
+			model(boost::lexical_cast<double>(argv[i]));
+		}
+
+		std::cerr << "count: " << count(model) << std::endl;
+		std::cerr << "min: " << min(model) << std::endl;
+		std::cerr << "mean: " << mean(model) << std::endl;
+		std::cerr << "median: " << median(model) << std::endl;
+		std::cerr << "max: " << max(model) << std::endl;
+		std::cerr << "sd: " << std::sqrt(variance(model)) << std::endl;
+
+		return 0;
+	}
+	catch(std::exception& e)
+	{
+		std::cerr << "exception: " << e.what() << std::endl;
+		return 1;
+	}
+}
+
