2d3242d7b3b6e38ae4c973b8d39d53bd2b06a654
Timothy M. Shead <tshead@k-3d.com>
2010-10-09 18:14:43 -0600

    Add graphviz support to the wiki2asciidoc script.

--- share/k3d/guide/content/wiki2asciidoc.py
+++ share/k3d/guide/content/wiki2asciidoc.py
@@ -1,4 +1,5 @@
 from optparse import OptionParser
+from uuid import uuid4
 import re
 
 def main():
@@ -6,18 +7,26 @@ def main():
 	#parser.add_option("--skip-rows", type="int", dest="skip_rows", default="0", help="Number of lines/rows to skip when using --lines or --csv.	Default: %default")
 	(options, arguments) = parser.parse_args()
 
-	listing_markup = "------------------------------------------------------"
+	listing_markup = "------------------------------------------------------------------\n"
 
 	for path in arguments:
 		tag_listing = False
 		block_listing = False
+		graphviz_listing = False
 		for markup in open(path, "r"):
 			if re.search("<pre>", markup):
 				tag_listing = True
-				markup = re.sub("<pre>", listing_markup, markup)
+				markup = listing_markup
 			elif re.search("</pre>", markup):
 				tag_listing = False
-				markup = re.sub("</pre>", listing_markup, markup)
+				markup = listing_markup
+			elif re.search("<graphviz>", markup):
+				graphviz_listing = True
+				print "[\"graphviz\", \"%s.png\"]" % str(uuid4())
+				markup = listing_markup
+			elif re.search("</graphviz>", markup):
+				graphviz_listing = False
+				markup = listing_markup
 			elif markup.startswith(" ") and not block_listing and not tag_listing:
 				print listing_markup
 				block_listing = True
@@ -26,7 +35,7 @@ def main():
 				print listing_markup
 				block_listing = False
 
-			if not tag_listing and not block_listing:
+			if not tag_listing and not block_listing and not graphviz_listing:
 				# Convert varying numbers of single-quotes to Asciidoc "emphasis"
 				markup = re.sub("('{2,5})([^']+('[^']+)*)\\1", "'\\2'", markup)
 				# Markup double-quotes
